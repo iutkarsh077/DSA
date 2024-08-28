76cabc23022f7ac9ed988cd0d06d746609c809ba
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-08-28 09:00:26 +0530

    NGE2

--- Stack/NextGreatestElement2.cpp
+++ Stack/NextGreatestElement2.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+/*int main() {
     vector<int> nums = {1,5,3,6,8};
     vector<int> result;
     for(int i = 0; i < nums.size(); i++){
@@ -23,5 +23,31 @@ int main() {
     for(int i = 0; i < result.size(); i++){
         cout << result[i] << " ";
     }
+    return 0;
+}*/
+
+
+int main() {
+    vector<int> nums = {1, 2, 1};
+    vector<int> result(nums.size(), -1); // Initialize result with the same size as nums
+    stack<int> st;
+
+    for(int i = (2 * nums.size()) - 1; i >= 0; i--) {
+        while(!st.empty() && st.top() <= nums[i % nums.size()]) {
+            st.pop();
+        }
+
+        if(i < nums.size()) {
+            result[i] = st.empty() ? -1 : st.top();
+        }
+
+        st.push(nums[i % nums.size()]);
+    }
+
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
