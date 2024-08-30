7580aadd513726f18c422c0a6e9b55e2b0f13f2e
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-08-30 20:39:50 +0530

    Bubble sort revision

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,9 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
 int main()
 {
     int arr[] = {2, 1, 7, 5, 8, 4, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    int i = 0;
+    while (i < n)
+    {
+        cout << arr[i] << " ";
+        i++;
+    }
+    return 0;
+}*/
+
+
+
+int main(){
+     int arr[] = {2, 1, 7, 5, 8, 4, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     for (int i = 0; i < n; i++)
--- Stack/Subarray_Min.cpp
+++ Stack/Subarray_Min.cpp
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    vector<int> arr = {1, 2};
-    long long sum = 0;
+vector<int> prevSmallElement(vector<int> nums) {
+    vector<int> result(nums.size(), -1);
+    stack<int> st;
 
-    for (int i = 0; i < arr.size(); i++)
-    {
-        int mini = arr[i];
-        for (int j = i; j < arr.size(); j++)
-        {
-            mini = min(arr[j], mini);
-            sum = sum + mini;
+    for (int i = 0; i < nums.size(); i++) {
+        while (!st.empty() && nums[st.top()] >= nums[i]) {
+            st.pop();
         }
+
+        if (!st.empty()) {
+            result[i] = st.top();
+        }
+
+        st.push(i);
     }
-    cout << sum << endl;
+
+    return result;
+}
+
+int main() {
+    vector<int> nums = {3, 1, 2, 4};
+    vector<int> result = prevSmallElement(nums);
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
     return 0;
 }
