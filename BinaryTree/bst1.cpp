38bf8ea69d37b99d6e9d195f4c5ddb7e49969414
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-08-31 14:28:51 +0530

    BST revision

--- BinaryTree/bst1.cpp
+++ BinaryTree/bst1.cpp
@@ -3,6 +3,138 @@
 #include <string>
 using namespace std;
 
-int main() {
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        this->data = d;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+Node *BuildTree(Node *root)
+{
+    int d;
+    cout << "Enter data: " << endl;
+    cin >> d;
+    root = new Node(d);
+
+    if (d == -1)
+    {
+        return nullptr;
+    }
+
+    cout << "Enter the data in left Node: " << d << endl;
+    root->left = BuildTree(root->left);
+    cout << "Enter the data in right Node: " << d << endl;
+    root->right = BuildTree(root->right);
+
+    return root;
+}
+
+/*void LevelOrderTransversal(Node *root)
+{
+    queue<Node *> que;
+    que.push(root);
+    que.push(nullptr);
+
+    while (!que.empty())
+    {
+        Node *temp = que.front();
+        que.pop();
+
+        if (temp == nullptr)
+        {
+            cout << endl;
+            if (!que.empty())
+            {
+                que.push(nullptr);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                que.push(temp->left);
+            }
+
+            if (temp->right)
+            {
+                que.push(temp->right);
+            }
+        }
+    }
+}*/
+
+/*void TransversalTree(Node *root){
+    if(root == nullptr){
+        return;
+    }
+
+    cout << root->data << " ";
+    TransversalTree(root->left);
+    TransversalTree(root->right);
+    // cout << endl;
+}*/
+
+/*void LevelOrderTransversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(nullptr);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == nullptr)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(nullptr);
+            }
+        }
+
+        else
+        {
+            cout << temp->data << ' ';
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}*/
+
+void Inorder(Node *root){
+    if(root == nullptr){
+        return;
+    }
+
+    Inorder(root->left);
+    cout << root->data << " ";
+    Inorder(root->right);
+}
+int main()
+{
+    Node *root = nullptr;
+    root = BuildTree(root);
+    // TransversalTree(root);
+    Inorder(root);
     return 0;
-} 
+}
+
+// 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
