548fd9b23ecb0ce8bd3bd83d702e6e08058e332e
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-10 11:38:21 +0530

    delete from bst

--- BST/SearchInBST.cpp
+++ BST/SearchInBST.cpp
@@ -34,6 +34,59 @@ bool searchInBST(Node *root, int x) {
         return searchInBST(root->left, x);
     }
 }
+int minVal(Node *root){
+    if(root->left == nullptr){
+        return root->data;
+    }
+
+    int val = minVal(root->left);
+    return val;
+}
+
+Node *deleteFromBST(Node *root, int val){
+    if(root == nullptr){
+        return nullptr;
+    }
+
+    if(root->data == val){
+        //Zero child
+        if(root->left == nullptr && root->right == nullptr){
+            delete root;
+            return nullptr;
+        }
+
+        // One Child
+        if(root->left != nullptr && root->right == nullptr){
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        if(root->right != nullptr && root->left == nullptr){
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        //Two child
+        else if(root->left != nullptr && root->right != nullptr){
+            int mini = minVal(root->right);
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+
+    else if(root->data > val){
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+
+    else{
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
 
 int main() {
     return 0;
