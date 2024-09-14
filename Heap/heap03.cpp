cd39cffa463cd6a5c225668eb68c20f15422ea62
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-14 09:14:16 +0530

    deletion in heap

--- Heap/heap03.cpp
+++ Heap/heap03.cpp
@@ -79,11 +79,47 @@ public:
         }
     }
 
-    void print(){
-        for(int i = 1; i <= size; i++){
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
             cout << arr[i] << " ";
         }
         cout << endl;
+    }
+
+    void deleteRootNode()
+    {
+        if (size == 0)
+        {
+            cout << "Heap is empty";
+            return;
+        }
+        swap(arr[1], arr[size]);
+        size--;
+
+        int i = 1;
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = (2 * i) + 1;
+
+            if (leftIndex < size && arr[i] < arr[leftIndex])
+            {
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }
+
+            else if (rightIndex < size && arr[i] < arr[rightIndex])
+            {
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }
+            else
+            {
+                return;
+            }
+        }
     }
 };
 
@@ -92,10 +128,12 @@ int main()
     MaxHeap heap;
     heap.insert(60);
     heap.insert(50);
+    heap.insert(40);
     heap.insert(30);
     heap.insert(20);
-    heap.insert(40);
     heap.insert(55);
+    heap.print();
+    heap.deleteRootNode();
     heap.print();
     return 0;
 }
