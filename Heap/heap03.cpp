475c87a72a46b8cc4949ea8f9c4563149689b220
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-14 08:53:36 +0530

    insert in heap

--- Heap/heap03.cpp
+++ Heap/heap03.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-class MaxHeap
+/*class MaxHeap
 {
 public:
     int arr[100];
@@ -44,16 +44,58 @@ public:
         }
     }
 };
+*/
+
+class MaxHeap
+{
+public:
+    int arr[100];
+    int size;
+
+    MaxHeap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void print(){
+        for(int i = 1; i <= size; i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
 
 int main()
 {
-    MaxHeap maxi;
-    maxi.insert(60);
-    maxi.insert(50);
-    maxi.insert(30);
-    maxi.insert(20);
-    maxi.insert(40);
-    maxi.insert(70);
-    maxi.Print();
+    MaxHeap heap;
+    heap.insert(60);
+    heap.insert(50);
+    heap.insert(30);
+    heap.insert(20);
+    heap.insert(40);
+    heap.insert(55);
+    heap.print();
     return 0;
 }
