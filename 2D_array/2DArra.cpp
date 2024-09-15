65a9b1b17682d359432113e5572fd4d15553abaa
UTKARSH SINGH <119475896+iutkarsh077@users.noreply.github.com>
2024-09-15 14:00:38 +0530

    Update 2DArra.cpp

--- 2D_array/2DArra.cpp
+++ 2D_array/2DArra.cpp
@@ -35,5 +35,14 @@ int main()
         }
         cout << endl;
     }
+
+     for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
