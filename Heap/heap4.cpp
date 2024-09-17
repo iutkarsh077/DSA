281d199887b55e46649b05a64a9bebcead8f9edf
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-17 22:44:15 +0530

    heapify algo

--- Heap/heap4.cpp
+++ Heap/heap4.cpp
@@ -76,7 +76,7 @@ int main() {
     h.deleteElement();
     h.print();
     return 0;
-}*/
+}
 
 class heap
 {
@@ -113,30 +113,36 @@ public:
         }
     }
 
-    void deleteElement(){
-        if(size == 0){
+    void deleteElement()
+    {
+        if (size == 0)
+        {
             cout << "Element is deleted" << endl;
             return;
         }
 
-       arr[1] = arr[size];
+        arr[1] = arr[size];
         size--;
 
         int i = 1;
-        while(i < size){
+        while (i < size)
+        {
             int leftIndex = 2 * i;
             int rightIndex = 2 * i + 1;
 
-            if(i < size && arr[i] < arr[leftIndex]){
+            if (i < size && arr[i] < arr[leftIndex])
+            {
                 swap(arr[i], arr[leftIndex]);
                 i = leftIndex;
             }
 
-            else if(i < size && arr[i] < arr[rightIndex]){
+            else if (i < size && arr[i] < arr[rightIndex])
+            {
                 swap(arr[i], arr[rightIndex]);
                 i = rightIndex;
             }
-            else{
+            else
+            {
                 return;
             }
         }
@@ -152,6 +158,29 @@ public:
     }
 };
 
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = (2 * i) + 1;
+
+    if (left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+
+    if (right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, i);
+    }
+}
+
 int main()
 {
     heap h;
@@ -163,5 +192,149 @@ int main()
     h.print();
     h.deleteElement();
     h.print();
+
+    int arr[] = {-1, 54, 53, 55, 52, 50};
+
+    int n = (sizeof(arr) / sizeof(arr[0])) - 1;
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    for(int i = 1; i <= n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}*/
+
+class heap
+{
+public:
+    int arr[100];
+    int size;
+
+    heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+
+        while (index > 1)
+        {
+            int parent = index / 2;
+
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void deleteElement()
+    {
+        if (size == 0)
+        {
+            cout << "Already empty: " << endl;
+            return;
+        }
+
+        arr[1] = arr[size];
+        size--;
+        int i = 1;
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
+
+            if (leftIndex < size && arr[i] < arr[leftIndex])
+            {
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }
+
+            else if (rightIndex < size && arr[i] < arr[rightIndex])
+            {
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+
+    if (right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+int main()
+{
+    heap h;
+    h.insert(60);
+    h.insert(50);
+    h.insert(40);
+    h.insert(30);
+    h.insert(20);
+    h.insert(55);
+    h.print();
+    h.deleteElement();
+    h.print();
+
+    int arr[] = {-1, 54, 53, 55, 52, 50};
+
+    int n = (sizeof(arr) / sizeof(arr[0])) - 1;
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
     return 0;
 }
