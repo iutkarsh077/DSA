6ea9a6bf72dddf694bcf2a81e423869a6abf4807
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-17 21:32:31 +0530

    heap 4

--- Heap/heap03.cpp
+++ Heap/heap03.cpp
@@ -123,17 +123,60 @@ public:
     }
 };
 
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+
+    if (right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n){
+    int size = n;
+
+    while(size > 1){
+        swap(arr[size], arr[1]);
+        size--;
+
+        heapify(arr, size, 1);
+    }
+}
+
 int main()
 {
     MaxHeap heap;
-    heap.insert(60);
-    heap.insert(50);
-    heap.insert(40);
-    heap.insert(30);
-    heap.insert(20);
-    heap.insert(55);
-    heap.print();
-    heap.deleteRootNode();
-    heap.print();
+    // heap.insert(60);
+    // heap.insert(50);
+    // heap.insert(40);
+    // heap.insert(30);
+    // heap.insert(20);
+    // heap.insert(55);
+    // heap.print();
+    // heap.deleteRootNode();
+    // heap.print();
+    int arr[] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+    for(int i = n / 2; i > 0; i--){
+        heapify(arr, n, i);
+    }
+
+    for(int i = 1; i <= n; i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
