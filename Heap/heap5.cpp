931c6c07cf4a0819033c9b3b58338c9c6ddc7078
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-19 07:36:43 +0530

    heap 5

---
+++ Heap/heap5.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+using namespace std;
+
+class heap
+{
+public:
+    int arr[100];
+    int size;
+
+    heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+
+        while (index > 1)
+        {
+            int parent = index / 2;
+
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void DeleteRoot()
+    {
+        if (size == 0)
+        {
+            cout << "Already empty: " << endl;
+            return;
+        }
+        swap(arr[1], arr[size]);
+        size--;
+
+        int i = 1;
+
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
+
+            if (leftIndex < size && arr[leftIndex] > arr[i])
+            {
+                swap(arr[leftIndex], arr[i]);
+                i = leftIndex;
+            }
+            else if (rightIndex < size && arr[rightIndex] > arr[i])
+            {
+                swap(arr[rightIndex], arr[i]);
+                i = rightIndex;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+
+    if (right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+int main()
+{
+    /*heap h;
+    h.insert(60);
+    h.insert(50);
+    h.insert(40);
+    h.insert(30);
+    h.insert(20);
+    h.print();
+    h.DeleteRoot();
+    h.print();*/
+
+    /*int arr[] = {-1, 3, 1, 5, 7, 8, 9, 6};
+
+    int n = (sizeof(arr) / sizeof(arr[0])) - 1;
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }*/
+
+
+   
+    return 0;
+}
