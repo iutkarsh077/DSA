7fc2a1932cbbdf4d4f743857c0da1a0a0b542f63
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-25 06:11:19 +0530

    patter que 22

---
+++ AgainRevision/patterns/Ninja_Srtar_pattern_1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n = 3;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j == 0 || j == n - 1 || i == 0 || i == n - 1)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
