90738c4bdcb73ba19a006ff1c35aa2c827bef194
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-09-27 17:24:31 +0530

    recursion

---
+++ AgainRevision/Recursion/onetoten.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNos(int N)
+{
+    // Your code here
+    if (N <= 0)
+    {
+        return;
+    }
+
+    printNos(N - 1);
+    cout << N << " ";
+}
+
+int main()
+{
+    printNos(0);
+    return 0;
+}
