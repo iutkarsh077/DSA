fccd23964abe049b742a83725d8b8bb88d35b8d2
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-02 08:22:49 +0530

    insertion and bubble sort

---
+++ AgainRevision/sorting/bubbleSort.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
