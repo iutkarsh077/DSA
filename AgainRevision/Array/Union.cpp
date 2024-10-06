37bb075cb60477053ac5260f34790ca506a79723
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-06 11:15:34 +0530

    Union three approaches

--- AgainRevision/Array/Union.cpp
+++ AgainRevision/Array/Union.cpp
@@ -37,18 +37,23 @@ void BetterMethode(int arr1[], int arr2[])
         result.push_back(arr1[i]);
     }
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 7; i++)
     {
         result.push_back(arr2[i]);
     }
 
     sort(result.begin(), result.end());
 
-    for (int i = 0; i < result.size() - 1; i++)
+    int i = 0;
+    while (i < result.size() - 1)
     {
         if (result[i] == result[i + 1])
         {
             result.erase(result.begin() + i + 1);
+        }
+        else
+        {
+            i++;
         }
     }
 
@@ -58,11 +63,135 @@ void BetterMethode(int arr1[], int arr2[])
     }
 }
 
+void OptimalApproach(int arr1[], int arr2[], int n, int m)
+{
+    int first = 0;
+    int second = 0;
+
+    vector<int> result;
+
+    // Merge and eliminate duplicates
+    while (first < n && second < m)
+    {
+        // If arr1 element is smaller or equal, add to result if not already present
+        if (arr1[first] <= arr2[second])
+        {
+            if (result.empty() || result.back() != arr1[first])
+            {
+                result.push_back(arr1[first]);
+            }
+            first++;
+            // Skip duplicate elements in both arrays
+            if (arr1[first - 1] == arr2[second])
+            {
+                second++;
+            }
+        }
+        // If arr2 element is smaller, add to result if not already present
+        else
+        {
+            if (result.empty() || result.back() != arr2[second])
+            {
+                result.push_back(arr2[second]);
+            }
+            second++;
+        }
+    }
+
+    // Handle remaining elements in arr1
+    while (first < n)
+    {
+        if (result.empty() || result.back() != arr1[first])
+        {
+            result.push_back(arr1[first]);
+        }
+        first++;
+    }
+
+    // Handle remaining elements in arr2
+    while (second < m)
+    {
+        if (result.empty() || result.back() != arr2[second])
+        {
+            result.push_back(arr2[second]);
+        }
+        second++;
+    }
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+}
+
+void OptimalApproachRev(int arr1[], int arr2[], int m, int n)
+{
+    int first = 0;
+    int second = 0;
+
+    vector<int> result;
+
+    while (first < m && second < n)
+    {
+        if (arr1[first] <= arr2[second])
+        {
+            if (result.empty() || result.back() != arr1[first])
+            {
+                result.push_back(arr1[first]);
+            }
+            first++;
+
+            if (arr1[first - 1] == arr2[second])
+            {
+                second++;
+            }
+        }
+
+        else
+        {
+            if (result.empty() || result.back() != arr2[second])
+            {
+                result.push_back(arr2[second]);
+            }
+            second++;
+        }
+    }
+
+    while (first < m)
+    {
+        if (result.empty() || result.back() != arr1[first])
+        {
+            result.push_back(arr1[first]);
+        }
+        first++;
+    }
+
+    while (second < n)
+    {
+        if (result.empty() || result.back() != arr2[second])
+        {
+            result.push_back(arr2[second]);
+        }
+        second++;
+    }
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    int arr1[] = {1, 2, 3, 4, 5};
-    int arr2[] = {1, 2, 3, 6, 7};
+    int arr1[] = {1, 1, 1, 1, 1};
+    int arr2[] = {2, 3, 5, 5, 5, 6, 7};
+    BetterMethode(arr1, arr2);
+    cout << endl;
+    OptimalApproach(arr1, arr2, 5, 7);
+    cout << endl;
+    OptimalApproachRev(arr1, arr2, 5, 7);
 
-    
     return 0;
 }
