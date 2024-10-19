d60350d8d5c61a7a59de7ec74b061147d221de8e
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-19 19:51:30 +0530

    kadane alogo

--- AgainRevision/Array/KadaneAlgo.cpp
+++ AgainRevision/Array/KadaneAlgo.cpp
@@ -20,6 +20,25 @@ int maxSubArray(vector<int> &nums)
     return maxi;
 }
 
+int maxSubArray2(vector<int> &nums)
+{
+    int sum = 0;
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+
+        maxi = max(sum, maxi);
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+
+    return maxi;
+}
+
 int main()
 {
     return 0;
