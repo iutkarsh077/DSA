cb76fdf6074b94cdde8090c7feaac1eb1c34f097
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-22 18:50:53 +0530

    longest

--- AgainRevision/Array/LongestSubarray12.cpp
+++ AgainRevision/Array/LongestSubarray12.cpp
@@ -25,6 +25,31 @@ int maxLenBrute(vector<int> &arr, int n)
     return maxLength;
 }
 
+int Optimal(vector<int>& arr, int n) {
+        // Your code here
+        int maxi = 0;
+        int sum = 0;
+        map<int, int> mp;
+        for(int i = 0; i < n; i++){
+            sum = sum + arr[i];
+            if(sum == 0){
+                maxi = i + 1;
+            }
+            
+            else{
+                if(mp.find(sum) != mp.end()){
+                    maxi = max(maxi, i - mp[sum]);
+                }
+                else{
+                    mp[sum] = i;
+                }
+            }
+        }
+        
+        return maxi;
+    }
+
+
 
  int Optimal(vector<int>& arr, int n) {
         // Your code here
