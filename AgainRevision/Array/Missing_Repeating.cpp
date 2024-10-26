741d360b289c451ccbd1fd14312e2fc80d6a683f
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-26 08:59:38 +0530

    mssing number

--- AgainRevision/Array/Missing_Repeating.cpp
+++ AgainRevision/Array/Missing_Repeating.cpp
@@ -26,6 +26,30 @@ using namespace std;
     return { repeating, missing };
 }
 
+vector<int> Rev(vector<int>& arr) {
+        // code here
+        vector<int> hashed(arr.size() + 1, 0);
+        
+        for(int i = 0; i < arr.size(); i++){
+            hashed[arr[i]]++;
+        }
+        
+        int repeating = 0;
+        int missing = 0;
+        
+        for(int i = 0; i < hashed.size(); i++){
+            if(hashed[i] == 2){
+                repeating = i;
+            }
+            
+            if(hashed[i] == 0){
+                missing = i;
+            }
+        }
+        
+        return { repeating, missing };
+    }
+
 int main() {
     return 0;
 }
