ecc74d5aab8b8a09cf76941c9c8ac568b5a40bcc
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-11-02 10:49:52 +0530

    Binary Search on SQRT

--- AgainRevision/BinarySearch/squareRoot.cpp
+++ AgainRevision/BinarySearch/squareRoot.cpp
@@ -19,5 +19,33 @@ long long int floorSqrt(long long int n)
 
 int main()
 {
+
+    long long int minVal = -1;
+    long long int n = 17123044;
+    if (n == 1)
+    {
+        cout << 1;
+        return 0;
+    }
+
+    long long int s = 0;
+    long long int e = n / 2;
+    while (s <= e)
+    {
+        int mid = s + ((e - s) / 2);
+
+        if (mid * mid <= n)
+        {
+            minVal = mid;
+            s = mid + 1;
+        }
+
+        else if (mid * mid > n)
+        {
+            e = mid - 1;
+        }
+    }
+
+    cout << minVal;
     return 0;
 }
