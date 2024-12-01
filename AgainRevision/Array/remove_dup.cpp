536084ef9d4411bbebfabe7ca1d2f65d29c262fa
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-01 14:12:49 +0530

    1 Dec 2024

--- AgainRevision/Array/remove_dup.cpp
+++ AgainRevision/Array/remove_dup.cpp
@@ -206,6 +206,30 @@ public:
             cout << result[i] << " ";
         }
     }
+
+    int singleNumber(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            bool flag = false;
+            for (int j = 0; j < nums.size(); j++)
+            {
+                if (i == j)
+                    continue;
+                if (nums[i] == nums[j])
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == false)
+            {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
 };
 
 int main()
