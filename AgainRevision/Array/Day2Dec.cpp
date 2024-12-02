38efbb90fd6aec3eade39e80a31b62efc5cc78a8
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-02 16:34:34 +0530

    Sort 0, 1, 2

--- AgainRevision/Array/Day2Dec.cpp
+++ AgainRevision/Array/Day2Dec.cpp
@@ -34,13 +34,59 @@ public:
         cout << maxi;
     }
 
-    
+    void sortColors(vector<int> &nums)
+    {
+        int zero = 0;
+        int one = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+            {
+                zero++;
+            }
+
+            else if (nums[i] == 1)
+            {
+                one++;
+            }
+        }
+
+        int two = nums.size() - (one + zero);
+        nums.clear();
+        while (one > 0 || two > 0 || zero > 0)
+        {
+            if (zero > 0)
+            {
+                nums.push_back(0);
+                zero--;
+            }
+
+            else if (one > 0)
+            {
+                nums.push_back(1);
+                one--;
+            }
+
+            else
+            {
+                nums.push_back(2);
+                two--;
+            }
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            cout << nums[i] << " ";
+        }
+        return;
+    }
 };
 
 int main()
 {
     ArrayProblem arr;
-    vector<int> arr1 = {-1, 2, -3};
-    arr.SubArrayProblem(arr1, -2);
+    vector<int> arr1 = {2, 0, 2, 1, 1, 0};
+    // arr.SubArrayProblem(arr1, -2);
+    arr.sortColors(arr1);
     return 0;
 }
