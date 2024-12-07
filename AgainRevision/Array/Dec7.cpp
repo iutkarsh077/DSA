590e0f210d1e0a75921c698115f445111b9f9c1f
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-07 15:20:36 +0530

    set zeros

--- AgainRevision/Array/Dec7.cpp
+++ AgainRevision/Array/Dec7.cpp
@@ -41,6 +41,35 @@ public:
         }
         return maxi;
     }
+
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        vector<int> rows(matrix.size(), 0);
+        vector<int> col(matrix[0].size(), 0);
+
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[i].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    rows[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[i].size(); j++)
+            {
+                if (rows[i] == 1 || col[j] == 1)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 };
 
 int main()
