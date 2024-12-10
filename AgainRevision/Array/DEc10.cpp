afc5f70ff9d073d6be4c5fc1105ce8215ecd0893
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-10 19:53:46 +0530

    n/3 majority element

--- AgainRevision/Array/DEc10.cpp
+++ AgainRevision/Array/DEc10.cpp
@@ -9,7 +9,8 @@ public:
         vector<vector<int>> result;
         result.push_back({1});
 
-        if(numRows == 1){
+        if (numRows == 1)
+        {
             return result;
         }
 
@@ -18,10 +19,11 @@ public:
             vector<int> val;
             for (int j = 0; j < i + 1; j++)
             {
-                if(j == 0 || j == i){
+                if (j == 0 || j == i)
+                {
                     val.push_back(1);
                 }
-                else 
+                else
                 {
                     int digit = result[i - 1][j - 1] + result[i - 1][j];
                     val.push_back(digit);
@@ -31,17 +33,38 @@ public:
         }
         return result;
     }
+
+    vector<int> majorityElement(vector<int> &nums)
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+
+        vector<int> arr;
+
+        for (auto i = mp.begin(); i != mp.end(); i++)
+        {
+            if (i->second > nums.size() / 3)
+            {
+                arr.push_back(i->first);
+            }
+        }
+
+        return arr;
+    }
 };
 
 int main()
 {
     ArrayProblem arr;
-    vector<vector<int>> result = arr.generate(5);
-
-    for(int i = 0; i < result.size(); i++){
-        for(int j = 0; j < result[i].size(); j++){
-            cout << result[i][j] << " ";
-        }
+    // vector<vector<int>> result = arr.generate(5);
+    vector<int> nums = {3, 2, 3};
+    vector<int> result = arr.majorityElement(nums);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
         cout << endl;
     }
     return 0;
