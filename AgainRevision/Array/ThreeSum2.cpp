ab81565c0eee2df1fab9b415886fa386f54c947d
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-14 17:19:41 +0530

    three sum part 2

--- AgainRevision/Array/ThreeSum2.cpp
+++ AgainRevision/Array/ThreeSum2.cpp
@@ -33,6 +33,17 @@ int main()
         }
     }
 
+       for (int i = 0; i < nums.size() - 2; i++) {
+        for (int j = i + 1; j < nums.size() - 1; j++) {
+            for (int k = j + 1; k < nums.size(); k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    result.insert(temp);
+                }
+            }
+        }
+    }
+
     vector<vector<int>> ans;
     for(const auto& triplet : result){
         ans.push_back(triplet);
