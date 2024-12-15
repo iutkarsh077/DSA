3886d3dd67262f81af07114b660a56459afe55a3
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-15 15:02:04 +0530

    longest

--- AgainRevision/Array/LargestSubarray.cpp
+++ AgainRevision/Array/LargestSubarray.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class ArrayProblem
 {
 public:
-    int LongestSubarrayWithZero(vector<int> arr)
+    int LongestSubarrayWithZeroBrute(vector<int> arr)
     {
         int maxi = -1;
         int cnt = 0;
@@ -24,12 +24,41 @@ public:
         }
         return maxi;
     }
+
+    int LongestSubarrayWithZero(vector<int> nums)
+    {
+        unordered_map<int, int> mp;
+        int maxi = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum = sum + nums[i];
+            if (sum == 0)
+            {
+                maxi = i + 1;
+            }
+
+            else
+            {
+                if (mp.find(sum) != mp.end())
+                {
+                    maxi = max(maxi, i - mp[sum]);
+                }
+                else
+                {
+                    mp[sum] = i;
+                }
+            }
+        }
+
+        return maxi;
+    }
 };
 
 int main()
 {
     ArrayProblem arrP;
-    vector<int> nums = {0};
-    cout  << arrP.LongestSubarrayWithZero(nums);
+    vector<int> nums = {15, -2, 2, -8, 1, 7, 10, 23};
+    cout << arrP.LongestSubarrayWithZero(nums);
     return 0;
 }
