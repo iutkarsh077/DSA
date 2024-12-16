755b5b8d83fa0296ccf3ef91e4fabc6ae70037c9
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-16 14:14:57 +0530

    merge sorted array

--- AgainRevision/Array/LargestSubarray.cpp
+++ AgainRevision/Array/LargestSubarray.cpp
@@ -53,12 +53,73 @@ public:
 
         return maxi;
     }
+
+     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int index = nums1.size() - 1;
+        bool flag = true;
+        for (int i = nums1.size() - 1; i >= 0; i--) {
+            if (nums1[i] != 0) {
+                flag = false;
+                index = i + 1;
+                break;
+            }
+        }
+
+        if(flag == true){
+            index = 0;
+        }
+
+        
+        int second = 0;
+        while (second < nums2.size()) {
+            nums1[index] = nums2[second];
+            index++;
+            second++;
+        }
+
+        sort(nums1.begin(), nums1.end());
+    }
+
+    vector<vector<int>> mergeOld(vector<vector<int>> nums)
+    {
+        vector<vector<int>> result;
+        int i = 0;
+        while (i < nums.size() - 1)
+        {
+            if (nums[i][1] >= nums[i + 1][0])
+            {
+                result.push_back({nums[i][0], nums[i + 1][1]});
+                i = i + 2;
+            }
+            else
+            {
+                result.push_back({nums[i][0], nums[i][1]});
+                i = i + 1;
+            }
+        }
+        if (i < nums.size())
+        {
+            result.push_back({nums[i][0], nums[i][1]});
+        }
+        return result;
+    }
 };
 
 int main()
 {
     ArrayProblem arrP;
-    vector<int> nums = {15, -2, 2, -8, 1, 7, 10, 23};
-    cout << arrP.LongestSubarrayWithZero(nums);
+    // vector<int> nums = {15, -2, 2, -8, 1, 7, 10, 23};
+    // cout << arrP.LongestSubarrayWithZero(nums);
+    vector<vector<int>> nums = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+    vector<vector<int>> ans = arrP.mergeOld(nums);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
