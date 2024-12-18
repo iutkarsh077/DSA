c84b5d3936e2ff3f3d39232b420bfce27b0d8503
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-18 18:59:10 +0530

    Count Inversion

---
+++ AgainRevision/Array/CountInversion.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    
+    int cnt = 0;
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    int left = low;
+    int right = mid + 1;
+
+    vector<int> temp;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            cnt += mid - left + 1;
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+
+    int mid = low + ((high - low) / 2);
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+    
+    int inversionCount(vector<int> &arr) {
+        
+    mergeSort(arr, 0, arr.size() - 1);
+    return cnt;
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        int n;
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+        Solution obj;
+        cout << obj.inversionCount(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+};
+
+
+int main() {
+    return 0;
+}
