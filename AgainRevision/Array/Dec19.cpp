5a2a6172ef4fac833c3f79af2d670ef82ec57fdb
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-20 19:42:47 +0530

    maxProduct

--- AgainRevision/Array/Dec19.cpp
+++ AgainRevision/Array/Dec19.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 class ArrayProblem{
     public:
+
+     int maxProduct(vector<int>& nums) {
+
+        if(nums.size() == 1) return nums[0];
+        int maxi = 0;
+        int prefix = 1;
+        int suffix = 1;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[nums.size() - i - 1];
+            maxi = max(maxi, max(prefix, suffix));
+        }
+
+        return maxi;
+    }
+
     int reversePairs(vector<int> &nums){
 
         if(nums.size() == 1) return -1;
