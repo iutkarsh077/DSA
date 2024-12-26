e08fbbb1a0c426d3165a9e24beb7d0a64e08046f
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-26 09:30:22 +0530

    search in rotated array

--- AgainRevision/BinarySearch/floor.cpp
+++ AgainRevision/BinarySearch/floor.cpp
@@ -201,6 +201,43 @@ vector<int> searchRange(vector<int> &nums, int target)
     }
 
 
+ int search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() - 1;
+
+         if(nums.size() == 0) return -1;
+        
+        while(s <= e){
+            int mid = s + ((e - s)/2);
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[s] <= nums[mid]){
+                if(nums[mid] >= target && nums[s] <= target){
+                    e = mid - 1;
+                }
+                else{
+                    s = mid + 1;
+                }
+            }
+
+             if(nums[mid] < nums[e]){
+                if(nums[mid] <= target && nums[e] >= target){
+                    s = mid + 1;
+                }
+
+                else {
+                    e = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+
 int main()
 {
     vector<int> nums = {5, 7, 7, 8, 8, 10};
