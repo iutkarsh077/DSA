7d5aca62edaff265e92ba5172a90d8898feceb14
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-27 09:01:10 +0530

    search in rotated sorted array part 2

---
+++ AgainRevision/BinarySearch/SearchKro2.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchBrute(vector<int> &nums, int target)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool search(vector<int>& nums, int target) {
+        int s = 0;
+        int e= nums.size() - 1;
+
+        while(s <= e){
+            int mid = s + (( e - s )/2);
+
+            if(nums[mid] == target){
+                return true;
+            }
+
+            
+            if (nums[s] == nums[mid] && nums[mid] == nums[e]) {
+                s++;
+                e--;
+                continue;
+            }
+
+            if(nums[s] <= nums[mid]){
+                if(nums[s] <= target && nums[mid] >= target){
+                    e = mid - 1;
+                }
+                else{
+                    s = mid + 1;
+                }
+            }
+
+            if(nums[mid] <= nums[e]){
+                if(nums[mid] <= target && nums[e] >= target){
+                    s = mid + 1;
+                }
+                else{
+                    e = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+int main()
+{
+    return 0;
+}
