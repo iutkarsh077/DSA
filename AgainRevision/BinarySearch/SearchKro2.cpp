3825d1f41c64af3e17fee1ff3236869fd8bd2b9a
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-28 09:33:09 +0530

    dec 28

--- AgainRevision/BinarySearch/SearchKro2.cpp
+++ AgainRevision/BinarySearch/SearchKro2.cpp
@@ -13,45 +13,72 @@ bool searchBrute(vector<int> &nums, int target)
     return false;
 }
 
-bool search(vector<int>& nums, int target) {
-        int s = 0;
-        int e= nums.size() - 1;
+int findKRotationBrute(vector<int> &arr)
+{
+    // Code Here
+    int minIndex = 0;
+    int minVal = arr[0];
 
-        while(s <= e){
-            int mid = s + (( e - s )/2);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] < minVal)
+        {
+            minVal = arr[i];
+            minIndex = i;
+        }
+    }
 
-            if(nums[mid] == target){
-                return true;
+    return minIndex;
+}
+
+bool search(vector<int> &nums, int target)
+{
+    int s = 0;
+    int e = nums.size() - 1;
+
+    while (s <= e)
+    {
+        int mid = s + ((e - s) / 2);
+
+        if (nums[mid] == target)
+        {
+            return true;
+        }
+
+        if (nums[s] == nums[mid] && nums[mid] == nums[e])
+        {
+            s++;
+            e--;
+            continue;
+        }
+
+        if (nums[s] <= nums[mid])
+        {
+            if (nums[s] <= target && nums[mid] >= target)
+            {
+                e = mid - 1;
             }
-
-            
-            if (nums[s] == nums[mid] && nums[mid] == nums[e]) {
-                s++;
-                e--;
-                continue;
-            }
-
-            if(nums[s] <= nums[mid]){
-                if(nums[s] <= target && nums[mid] >= target){
-                    e = mid - 1;
-                }
-                else{
-                    s = mid + 1;
-                }
-            }
-
-            if(nums[mid] <= nums[e]){
-                if(nums[mid] <= target && nums[e] >= target){
-                    s = mid + 1;
-                }
-                else{
-                    e = mid - 1;
-                }
+            else
+            {
+                s = mid + 1;
             }
         }
 
-        return false;
+        if (nums[mid] <= nums[e])
+        {
+            if (nums[mid] <= target && nums[e] >= target)
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+        }
     }
+
+    return false;
+}
 
 int main()
 {
