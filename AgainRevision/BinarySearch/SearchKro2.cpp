990f5ee975247b8bb07eeeba3ffb9edb4923bc34
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-29 08:23:37 +0530

    Date 29 december 2024

--- AgainRevision/BinarySearch/SearchKro2.cpp
+++ AgainRevision/BinarySearch/SearchKro2.cpp
@@ -31,6 +31,72 @@ int findKRotationBrute(vector<int> &arr)
     return minIndex;
 }
 
+int singleNonDuplicate(vector<int> &nums)
+{
+    if (nums.size() == 1)
+    {
+        return nums[0];
+    }
+
+    if (nums[nums.size() - 1] != nums[nums.size() - 2])
+    {
+        return nums[nums.size() - 1];
+    }
+
+    if (nums[0] != nums[1])
+    {
+        return nums[0];
+    }
+
+    int s = 0;
+    int e = nums.size() - 1;
+
+    while (s <= e)
+    {
+        int mid = s + ((e - s) / 2);
+
+        if (mid > 0 && nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        {
+            return nums[mid];
+        }
+
+        if ((nums[mid] == nums[mid + 1] && mid % 2 == 0) || (nums[mid] == nums[mid - 1] && mid % 2 != 0))
+        {
+            s = mid + 1;
+        }
+
+        if ((nums[mid] == nums[mid - 1] && mid % 2 == 0) || (nums[mid] == nums[mid + 1] && mid % 2 != 0))
+        {
+            e = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int findPeakElementBrute(vector<int>& nums) {
+        if(nums.size() == 1){
+            return 0;
+        }
+
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+
+        if(nums[nums.size() - 1] > nums[nums.size() - 2]){
+            return nums.size() - 1;
+        }
+
+        for(int i = 1; i < nums.size() - 1; i++){
+            if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]){
+                return i;
+            }
+        }
+
+
+        return -1;
+    }
+
 bool search(vector<int> &nums, int target)
 {
     int s = 0;
