81ac8b6d316967c5979a7444faff72eda796fddf
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-01-04 09:07:53 +0530

    Done

--- AgainRevision/BinarySearch/Dec3January.cpp
+++ AgainRevision/BinarySearch/Dec3January.cpp
@@ -83,6 +83,34 @@ int smallestDivisor(vector<int> &nums, int threshold)
     return ans;
 }
 
+ int floorSqrt(int n) {
+         if (n == 0)
+    {
+        return 0;
+    }
+    int s = 1;
+    int e = n;
+    int result = 1;
+
+    while (s <= e)
+    {
+        int mid = s + ((e - s) / 2);
+        if(mid * mid == n){
+            return mid;
+        }
+
+        else if(mid * mid > n){
+            e = mid - 1;
+        }
+        else if(mid * mid < n){
+            s = mid + 1;
+            result = mid;
+        }
+    }
+
+    return result;
+    }
+
 int main()
 {
     vector<int> nums = {1, 2, 5, 9};
