35d98a992486297caacde62b10a143df9d35c8dc
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-01-09 08:14:36 +0530

    Je jatt vigad gya

--- AgainRevision/string/Jan8.cpp
+++ AgainRevision/string/Jan8.cpp
@@ -3,28 +3,51 @@
 #include <string>
 using namespace std;
 
-string reverseWords(string s) {
-        int i = 0;
-        string ans = "";
+string reverseWords(string s)
+{
+    int i = 0;
+    string ans = "";
 
-        while(i < s.length()){
-            string words = "";
-            int j = i;
-            while(j < s.length() && s[j] != ' '){
-                words += s[j];
-                j++;
-            }
-
-            if(words.length() > 0){
-                ans = words + ' ' + ans;
-            }
-            i = j + 1;
+    while (i < s.length())
+    {
+        string words = "";
+        int j = i;
+        while (j < s.length() && s[j] != ' ')
+        {
+            words += s[j];
+            j++;
         }
 
-        ans.pop_back();
-        return ans;
+        if (words.length() > 0)
+        {
+            ans = words + ' ' + ans;
+        }
+        i = j + 1;
     }
 
-int main() {
+    ans.pop_back();
+    return ans;
+}
+
+string largestOddNumber(string num)
+{
+    string ans = "";
+    int n = num.size();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int val = num[i] - '0';
+        if (val % 2 != 0)
+        {
+            ans = num.substr(0, i + 1);
+            break;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
     return 0;
 }
