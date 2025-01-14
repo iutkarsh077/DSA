9f03f0b7c014bd57ef065a4beb4b92ab8c671138
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-01-14 07:09:36 +0530

    max Depth

--- AgainRevision/string/str.cpp
+++ AgainRevision/string/str.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-int main()
+int val()
 {
     string name = "Utkarsh";
     cout << name.length() << endl;
@@ -18,4 +18,27 @@ int main()
     string str2 = name.substr(3, 2);
     cout << str2 << endl;
     return 0;
+}
+
+
+ int maxDepth(string s) {
+        int cnt = 0;
+        int maxi = INT_MIN;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                cnt++;
+            }
+
+            else if (s[i] == ')') {
+                cnt--;
+            }
+
+            maxi = max(cnt, maxi);
+        }
+        return maxi;
+    }
+
+int main(){
+
 }
