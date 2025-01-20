76aedfbf5aa078d18e16ae55dc894aef5b0dc894
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-01-20 07:46:38 +0530

    DLL

---
+++ AgainRevision/LinkedList/DDl1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = nullptr;
+        this->next = nullptr;
+    }
+};
+
+int main()
+{
+}
