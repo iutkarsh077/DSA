2d4b4ab252199311224e062abd698d3a1f2cb6f0
UTKARSH SINGH <119475896+iutkarsh077@users.noreply.github.com>
2025-01-22 09:01:39 +0530

    Update create_array.cpp

--- 2D_array/create_array.cpp
+++ 2D_array/create_array.cpp
@@ -14,6 +14,12 @@ int main() {
             cout << arr[i][j] << " ";
         }
         cout << endl;
+    
+for(int i=0; i<4; i++) {
+        for(int j=0; j<4; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
