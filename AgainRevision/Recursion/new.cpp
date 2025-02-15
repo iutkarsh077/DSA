0cac8266e87c18e1377f3c355597a4537966d600
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-02-15 08:07:33 +0530

    pow

---
+++ AgainRevision/Recursion/new.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 2;
+    int n = 10;
+    double ans = pow(x, n);
+    cout << ans;
+    return 0;
+}
