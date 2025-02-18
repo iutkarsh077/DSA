2b2f1a48e1c689b5a2170e477e698e9ba39491da
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-02-18 07:23:39 +0530

    pow

--- AgainRevision/Recursion/new.cpp
+++ AgainRevision/Recursion/new.cpp
@@ -21,6 +21,40 @@ bool isPrime(int n)
 	return ans;
 }
 
+double myPow(double x, int n)
+{
+    if (x == 0)
+    {
+        return 0;
+    }
+
+    double result = 1;
+    long long int duplicate = n;
+
+    if (duplicate < 0)
+    {
+        duplicate = -duplicate;
+        x = 1 / x;
+    }
+
+    while (duplicate > 0)
+    {
+        if (duplicate % 2 == 1)
+        {
+            result *= x;
+        }
+
+        x *= x;
+        duplicate /= 2;
+    }
+    return result;
+}
+
+int main()
+{
+    return 0;
+}
+
 int main()
 {
     int x = 2;
