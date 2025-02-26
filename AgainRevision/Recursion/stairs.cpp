c9454924d2832f4eac84a5d113d8a663ced548bc
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-02-26 08:21:03 +0530

    stairs

---
+++ AgainRevision/Recursion/stairs.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int countDistinctWays(int nStairs) {
+      if (nStairs == 0) 
+            return 1;
+
+        if (nStairs < 0) return 1;
+
+
+        int ans = countDistinctWays(nStairs - 1) +  countDistinctWays(nStairs - 2);
+
+        return ans;
+  }
+
+
+  int main(){
+    return 0;
+  }
