8aa980b624c7ec3f61b96a9a337866e257f172bf
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-02-28 06:50:23 +0530

    reverse string

---
+++ AgainRevision/Recursion/str1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+void printVal(string val, int i){
+    if(i < 0) return;
+
+    cout << val[i];
+
+    printVal(val, i - 1);
+}
+
+int main(){
+    string val = "abcde";
+    printVal(val, val.length());
+    return 0;
+}
