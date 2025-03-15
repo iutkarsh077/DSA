e3ad062ed6387bfc95e1becbc588ae50b08bc78c
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-03-15 07:04:40 +0530

    valid parenthesis

--- AgainRevision/stack/validParenthesis.cpp
+++ AgainRevision/stack/validParenthesis.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+/*
 bool isValidParenthesis(string s)
 {
     stack<char> st;
@@ -21,6 +22,30 @@ bool isValidParenthesis(string s)
             st.pop();
 
             if((alpha == '}' && myTop != '{') || (alpha == ')' && myTop != '(') || (alpha == ']' && myTop != '[')){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+    */
+
+
+bool validParenthesis(string str, int size){
+    stack<char> st;
+    
+    for(int i = 0; i < str.length(); i++){
+        char ch = str[i];
+
+        if(ch == '{' || ch == '(' || ch == '[') st.push(ch);
+
+        else if(ch == '}' || ch == ')' || ch == ']'){
+            if(st.empty()) return false;
+
+            char mytop = st.top();
+            st.pop();
+            if((ch == '}' && mytop != '{') || (ch == ')' && mytop != '(') || ((ch == ']' && mytop != '['))){
                 return false;
             }
         }
