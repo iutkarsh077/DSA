a130651f29d3f8d1bb13136cef63a16194a51f9a
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-03-16 07:06:55 +0530

    O(n^2) sort

--- AgainRevision/stack/sortStack.cpp
+++ AgainRevision/stack/sortStack.cpp
@@ -3,37 +3,81 @@
 #include <string>
 using namespace std;
 
-void SortStack(stack<int> &st){
+void SortStack(stack<int> &st)
+{
     vector<int> arr;
-    while(!st.empty()){
+    while (!st.empty())
+    {
         arr.push_back(st.top());
         st.pop();
     }
 
     sort(arr.begin(), arr.end());
-    for(int i = 0; i < arr.size(); i++){
+    for (int i = 0; i < arr.size(); i++)
+    {
         st.push(arr[i]);
     }
 }
 
-
-int main() {
+void Initial()
+{
     stack<int> st;
 
-  st.push(6);
-  st.push(2);
-  st.push(8);
-  st.push(9);
-  st.push(3);
-  st.push(78);
-  st.push(100);
+    st.push(6);
+    st.push(2);
+    st.push(8);
+    st.push(9);
+    st.push(3);
+    st.push(78);
+    st.push(100);
 
     SortStack(st);
 
-    while(!st.empty()){
+    while (!st.empty())
+    {
         cout << st.top() << " ";
         st.pop();
     }
+}
 
+void sortMe(int num, stack<int> &st){
+    if(st.empty() || st.top() <= num){
+        st.push(num);
+        return;
+    }
+
+    int val = st.top();
+    st.pop();
+
+    sortMe(num, st);
+    st.push(val);
+}
+
+void sortStack(stack<int> &st){
+    if(st.empty()) return;
+
+    int num = st.top();
+    st.pop();
+
+    sortStack(st);
+    sortMe(num, st);
+}
+
+int main()
+{
+    stack<int> st;
+    st.push(9);
+    st.push(4);
+    st.push(-2);
+    st.push(-4);
+    st.push(21);
+
+    sortStack(st);
+
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
     return 0;
 }
