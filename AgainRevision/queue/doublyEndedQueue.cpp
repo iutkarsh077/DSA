80b3561b833b8e5022898e63ea3b017989e282ed
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-03-24 08:07:50 +0530

    circular queue

--- AgainRevision/queue/doublyEndedQueue.cpp
+++ AgainRevision/queue/doublyEndedQueue.cpp
@@ -1,11 +1,12 @@
+/*
 #include <iostream>
 using namespace std;
 
-class DoublyQueue {
+class circularQueue {
 public:
     int *arr, front, rear, size;
 
-    DoublyQueue(int n) {
+    circularQueue(int n) {
         size = n;
         arr = new int[size];
         front = rear = -1;
@@ -34,7 +35,7 @@ public:
 };
 
 int main() {
-    DoublyQueue d(6);
+    circularQueue d(6);
     d.PushElement(1);
     d.PushElement(76);
     d.PushElement(45);
@@ -44,5 +45,76 @@ int main() {
 
     for (int i = 0; i < 7; i++) cout << d.popElement() << endl;
     d.PushElement(732);
+    return 0;
+}
+*/
+
+
+#include<iostream>
+using namespace std;
+
+class circularQueue{
+    public:
+    int *arr;
+    int front;
+    int rear;
+    int size;
+
+    circularQueue(int n){
+        this->size = n;
+        arr = new int[size];
+        front = rear = -1;
+    }
+
+
+    void pushElement(int d){
+        if((rear + 1) % size == front){
+            cout << "Queue Overflow";
+            return;
+        }
+
+        if(front == -1) front = 0;
+        rear++;
+        arr[rear] = d;
+    }
+
+    int popElement(){
+        if(front == -1){
+            cout << "Queue is empty";
+            return -1;
+        }
+
+        int ans = arr[front];
+        if(front == rear){
+            front = rear = -1;
+        }
+        else front = (front + 1) % size;
+        return ans;
+    }
+
+    void printAll(){
+        int i = front;
+        while (true) {
+            cout << arr[i] << " ";
+            if (i == rear) break;  
+            i = (i + 1) % size;
+        }
+    }
+};
+
+int main(){
+    circularQueue q(6);
+    q.pushElement(2);
+    q.pushElement(8);
+    q.pushElement(45);
+    q.pushElement(98);
+    q.pushElement(23);
+    q.pushElement(56);
+
+    cout << q.popElement() << endl;
+    cout << q.popElement() << endl;
+    cout << endl;
+    cout << endl;
+    q.printAll();
     return 0;
 }
