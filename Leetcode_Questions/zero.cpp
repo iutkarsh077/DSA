fc8195be1ad330eac3e51bf2ffed7a71ecbb59f5
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-04-01 06:30:21 +0530

    intersection

--- Leetcode_Questions/zero.cpp
+++ Leetcode_Questions/zero.cpp
@@ -29,6 +29,25 @@ void moveZeroes(vector<int>& nums) {
     return;
 }
 
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    set<int> s1;
+
+    for(int i = 0; i < nums1.size(); i++){
+        for(int j = 0; j < nums2.size(); j++){
+            if(nums1[i] == nums2[j]){
+                s1.insert(nums1[i]);
+            }
+        }
+    }
+
+
+    vector<int> ans;
+    for(auto i = s1.begin(); i != s1.end(); i++){
+        ans.push_back(*i);
+    }
+    return ans;
+}
+
 int main() {
     return 0;
 }
