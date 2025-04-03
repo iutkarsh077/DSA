a8ecb6a7d8820d4b7b2c5c2d729617273d19374e
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-04-03 06:59:26 +0530

    two sum

--- Leetcode_Questions/stackUsingQueue.cpp
+++ Leetcode_Questions/stackUsingQueue.cpp
@@ -16,7 +16,18 @@ public:
         rear = -1;
     }
 
+     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        for(int i = 0; i < nums.size() - 1; i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    return {i, j};
+                }
+            }
+        }
 
+        return { -1, -1 };
+    }
     
 
     void Enqueue(int data)
