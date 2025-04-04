a418a548962fef49f6f40c8b16895f934bc37b66
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2025-04-04 06:56:42 +0530

    kth smallest

--- Sheet/rev.cpp
+++ Sheet/rev.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 
 void reverseArray(vector<int> &arr) {
@@ -17,6 +18,33 @@ void reverseArray(vector<int> &arr) {
     return;
 }
 
+void kthSmallest(vector<int> arr, int k){
+    map<int, int> mpp;
+    for(int i = 0; i < arr.size(); i++){
+        mpp[arr[i]] = i;
+    }
+
+    int index = 0;
+    for(auto i = mpp.begin(); i != mpp.end(); i++){
+       if(index == k - 1){
+        cout << i->first;
+        break;
+       }
+       else{
+        index++;
+       }
+    }
+
+    cout << endl;
+    cout << endl;
+
+    for(auto i = mpp.begin(); i != mpp.end(); i++){
+        cout << i->first << " " << i->second << endl;
+    }
+}
+
 int main(){
+    vector<int> arr ={7, 10, 4, 3, 20, 15};
+    kthSmallest(arr, 3);
     return 0;
 }
