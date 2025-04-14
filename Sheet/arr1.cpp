1acbd61c717d38762b1b6bdf89d9be077cdae91c
utkarsh <utkarshsingh132003@gmail.com>
2025-04-14 07:34:40 +0530

    check kr

--- Sheet/arr1.cpp
+++ Sheet/arr1.cpp
@@ -40,6 +40,24 @@ int findKthLargest(vector<int> &nums, int k)
     return -1;
 }
 
+bool check(vector<int>& nums) {
+    int cnt = 0;
+
+    for(int i = 0; i < nums.size() - 1; i++){
+        if(nums[i] > nums[i + 1]){
+            cnt++;
+        }
+    }
+
+    if(nums[0] < nums[nums.size() - 1]){
+        cnt++;
+    }
+
+    if(cnt <= 1) return true;
+
+    return false;
+}
+
 int main()
 {
     map<int, int> mp;
