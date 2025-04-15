a607aa29cb9548fc633b31f2b739386a2ee4e9fc
utkarsh <utkarshsingh132003@gmail.com>
2025-04-15 06:55:44 +0530

    rotate me in left

--- Sheet/arr1.cpp
+++ Sheet/arr1.cpp
@@ -58,6 +58,14 @@ bool check(vector<int>& nums) {
     return false;
 }
 
+void rotate(vector<int>& nums, int k) {
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+    return;
+}
+
 int main()
 {
     map<int, int> mp;
