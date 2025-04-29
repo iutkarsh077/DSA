9806f719807023eb2f8d622c5eef02b2c5533c7a
utkarsh <utkarshsingh132003@gmail.com>
2025-04-29 07:05:24 +0530

    maxProfit better

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -17,6 +17,30 @@ int maxSubArray(vector<int>& nums) {
     return maxi;
 }
 
+
+int maxProfitBrute(vector<int>& prices) {
+    int maxi = 0;
+
+    for (int i = 0; i < prices.size() - 1; i++) {
+        for (int j = i + 1; j < prices.size(); j++) {
+            maxi = max(maxi, prices[j] - prices[i]);
+        }
+    }
+
+    return maxi;
+}
+
+int maxProfit(vector<int>& prices) {
+    int mini = INT_MAX;
+    int maxi = 0;
+    for(int i = 0; i < prices.size(); i++){
+        mini = min(mini, prices[i]);
+        maxi = max(maxi, prices[i] - mini);
+    }
+
+    return maxi;
+}
+
 int main(){
 
 }
