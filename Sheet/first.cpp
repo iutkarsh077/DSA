afa26afddd129533733eb7536f48f1ca9120de37
utkarsh <utkarshsingh132003@gmail.com>
2025-04-30 08:47:52 +0530

    Better

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -41,6 +41,25 @@ int maxProfit(vector<int>& prices) {
     return maxi;
 }
 
+vector<int> rearrangeArray(vector<int>& nums) {
+    vector<int> ans(nums.size(), 0);
+    int cntPos = 0;
+    int cntNeg = 1;
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] >= 0){
+            ans[cntPos] = nums[i];
+            cntPos = cntPos + 2; 
+        }
+
+        else{
+            ans[cntNeg] = nums[i];
+            cntNeg = cntNeg + 2; 
+        }
+    }
+
+    return ans;
+}
+
 int main(){
 
 }
