ce016d27ed6976b9c3d458bba11331321f6322e1
utkarsh <utkarshsingh132003@gmail.com>
2025-05-01 07:27:42 +0530

    leaders better

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -61,6 +61,20 @@ vector<int> rearrangeArray(vector<int>& nums) {
 }
 
 
+vector<int> leaders(vector<int>& arr) {
+    vector<int> ans;
+    int maxi = -1;
+    
+    for(int i = arr.size() - 1; i >= 0; i--){
+        if(arr[i] >= maxi){
+            ans.push_back(arr[i]);
+            maxi = arr[i];
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
 int main(){
 
 }
