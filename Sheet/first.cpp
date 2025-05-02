ed462261ef1be4deecb01c9484917605bde026aa
utkarsh <utkarshsingh132003@gmail.com>
2025-05-02 13:40:24 +0530

    longest

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -75,6 +75,38 @@ vector<int> leaders(vector<int>& arr) {
     return ans;
 }
 
+class Solution {
+    public:
+        int longestConsecutive(vector<int>& nums) {
+            sort(nums.begin(), nums.end());
+    
+            int cnt = 1;
+            int maxi = 1;
+    
+            if(nums.size() == 0){
+                return 0;
+            }
+    
+            if(nums.size() == 1){
+                return 1;
+            }
+    
+            for(int i = 0; i < nums.size() - 1; i++){
+                if(nums[i] + 1 == nums[i + 1]){
+                    cnt++;
+                    maxi = max(maxi, cnt);
+                }
+    
+                else if(nums[i] == nums[i + 1]) continue;
+                else if(nums[i] + 1 != nums[i + 1]){
+                    cnt = 1;
+                    maxi = max(cnt, maxi);
+                }
+            }
+            return maxi;
+        }
+    };
+
 int main(){
 
 }
