7a70d404a76092a62cb1a8ad827f32ff86a7456b
utkarsh <utkarshsingh132003@gmail.com>
2025-05-03 06:05:49 +0530

    matrix zero

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -107,6 +107,28 @@ class Solution {
         }
     };
 
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> rows(matrix.size(), 0);
+        vector<int> cols(matrix[0].size(), 0);
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size(); j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size(); j++){
+                if(rows[i] == 1 || cols[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
 int main(){
 
 }
