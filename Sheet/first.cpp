a65374deb9aabae08e3003c319a6f86f59ba95da
utkarsh <utkarshsingh132003@gmail.com>
2025-05-06 06:15:15 +0530

    Exam hai bhai aaj

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -182,6 +182,19 @@ class Solution {
         }
     }
 
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size() - 1; i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    return { i, j };
+                }
+            }
+        }
+
+        return { -1, -1 };
+    }
+
 int main(){
 
 }
