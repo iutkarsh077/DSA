f9d4299d9267e966ee1b6b46d74183191824f6f5
utkarsh <utkarshsingh132003@gmail.com>
2025-05-13 06:33:25 +0530

    better

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -507,6 +507,63 @@ public:
        return cnt;
     }
 
+
+    class Solution {
+  public:
+    // Function to count inversions in the array.
+    int cnt = 0;
+    void merge(int low, int mid,  int high, vector<int> &arr){
+        int left = low;
+        int right = mid + 1;
+        
+        vector<int> temp;
+        
+        while(left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            
+            else {
+                temp.push_back(arr[right]);
+                cnt += mid - left + 1;
+                right++;
+            }
+        }
+        
+        while(left <= mid){
+              temp.push_back(arr[left]);
+                left++;
+        }
+        
+        
+        while(right <= high){
+             temp.push_back(arr[right]);
+                right++;
+        }
+        
+        for(int i = low; i <= high; i++){
+            arr[i] = temp[i - low];
+        }
+    }
+    
+    
+    void divide(int low, int high, vector<int> &arr){
+        if(low >= high) return;
+        
+        int mid = low + ((high - low)/2);
+        divide(low, mid, arr);
+        divide(mid + 1, high, arr);
+        merge(low,mid,  high, arr);
+        
+    }
+    int inversionCount(vector<int> &arr) {
+        // Your Code Here
+        divide(0, arr.size() - 1, arr);
+        return cnt;
+    }
+};
+
 int main()
 {
 }
