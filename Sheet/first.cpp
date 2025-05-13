a28ac4b4321adbcf08f53a46e0a07780f6a3c35c
utkarsh <utkarshsingh132003@gmail.com>
2025-05-13 06:21:30 +0530

    brute

--- Sheet/first.cpp
+++ Sheet/first.cpp
@@ -493,6 +493,20 @@ public:
         return {repeated, ans};
     }
 
+
+    
+      int inversionCountBrute(vector<int> &arr) {
+       int cnt = 0;
+       for(int i = 0; i < arr.size() - 1; i++){
+           for(int j = i + 1; j < arr.size(); j++){
+               if(arr[i] > arr[j]){
+                   cnt++;
+               }
+           }
+       }
+       return cnt;
+    }
+
 int main()
 {
 }
