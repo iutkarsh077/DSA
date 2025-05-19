4ddb41a9bfab5eec84a5cbc4942373324a36518e
utkarsh <utkarshsingh132003@gmail.com>
2025-05-19 07:24:47 +0530

    Find nth root of m

--- Sheet/second.cpp
+++ Sheet/second.cpp
@@ -27,6 +27,33 @@ int mySqrt(int x) {
         return static_cast<int>(std::round(e));
     }
 
+
+     int nthRoot(int n, int m) {
+        // Code here.
+        if(n == 1) return m;
+        
+        int s = 1;
+        int e = m;
+        
+        while(s <= e){
+            int mid = s +((e - s)/2);
+            
+            long long val = pow(mid, n);
+            
+            if(val == m) return mid;
+            
+            else if(val > m){
+                e = mid - 1;
+            }
+            
+            else{
+                s = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+
 int main(){
      int ans = 8.7;
     cout << ans;
