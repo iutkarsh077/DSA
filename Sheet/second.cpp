792bde372aaed8c54cdbbae2bc74c9917a00ab9d
utkarsh <utkarshsingh132003@gmail.com>
2025-05-24 05:43:01 +0530

    binary dup

--- Sheet/second.cpp
+++ Sheet/second.cpp
@@ -54,6 +54,68 @@ int mySqrt(int x) {
         return -1;
     }
 
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+
+        int i = 0;
+        while(i < nums.size() - 1){
+            if(nums[i] != nums[i + 1]){
+                return nums[i];
+            }
+            else{
+                i = i + 2;
+            }
+        }
+
+        if(nums[nums.size() - 1] != nums[nums.size() - 2]){
+            return nums[nums.size() - 1];
+        }
+
+        return -1;
+    }
+
+     int singleNonDuplicateBinary(vector<int>& nums) {
+        if (nums.size() == 1)
+            return nums[0];
+
+        if (nums[0] != nums[1])
+            return nums[0];
+        int s = 1;
+        int e = nums.size() - 2;
+
+        while (s <= e) {
+            int mid = s + ((e - s) / 2);
+
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+
+            else if (nums[mid] == nums[mid - 1]) {
+                if ((mid - 1) % 2 == 0) {
+                    s = mid + 1;
+                }
+
+                else {
+                    e = mid - 1;
+                }
+            }
+
+            else if (nums[mid] == nums[mid + 1]) {
+                if (mid % 2 == 0) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
+        }
+
+        if(nums[nums.size() - 1] != nums[nums.size() - 2]){
+            return nums[nums.size() - 1];
+        }
+
+        return -1;
+    }
+
 int main(){
      int ans = 8.7;
     cout << ans;
