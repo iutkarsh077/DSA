9242b62703105397ff5c7b09cf541ae997b341b4
utkarsh <utkarshsingh132003@gmail.com>
2025-05-29 07:09:44 +0530

    1903

--- Sheet/second.cpp
+++ Sheet/second.cpp
@@ -165,6 +165,21 @@ int mySqrt(int x) {
         return ans;
     }
 
+
+     string largestOddNumber(string num) {
+        string ans = "";
+
+        for(int i = num.size() - 1; i >= 0; i--){
+            int digit = num[i] - '0';
+
+            if(digit % 2 != 0){
+                ans = num.substr(0, i  + 1);
+                break;
+            }
+        }
+
+        return ans;
+    }
     class Solution {
 public:
     long long calculateHours(vector<int> piles, int hour){
