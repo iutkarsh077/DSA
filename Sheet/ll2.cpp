67179859c236c8e65a9c73eac508bea20a4e2158
utkarsh <utkarshsingh132003@gmail.com>
2025-06-09 06:06:14 +0530

    recursive approach

--- Sheet/ll2.cpp
+++ Sheet/ll2.cpp
@@ -10,7 +10,7 @@ public:
     ListNode(int data)
     {
         this->val = data;
-        this->next = nullptr
+        this->next = nullptr;
     }
 };
 
@@ -41,20 +41,48 @@ ListNode *middleNode(ListNode *head)
     return slow;
 }
 
- ListNode* reverseList(ListNode* head) {
-        if(head == nullptr) return nullptr;
+ListNode *reverseList(ListNode *head)
+{
+    if (head == nullptr)
+        return nullptr;
 
-        ListNode *dupHead = new ListNode(head->val);
-        ListNode *tracker = head->next;
-        while(tracker != nullptr){
-            ListNode *newHead = new ListNode(tracker->val);
-            newHead->next = dupHead;
-            dupHead = newHead;
-            tracker = tracker->next;
-        }
-
-        return dupHead;
+    ListNode *dupHead = new ListNode(head->val);
+    ListNode *tracker = head->next;
+    while (tracker != nullptr)
+    {
+        ListNode *newHead = new ListNode(tracker->val);
+        newHead->next = dupHead;
+        dupHead = newHead;
+        tracker = tracker->next;
     }
+
+    return dupHead;
+}
+
+void custom(ListNode *tracker, ListNode *&dupHead)
+{
+    if (tracker == nullptr)
+        return;
+
+    ListNode *newHead = new ListNode(tracker->val);
+    newHead->next = dupHead;
+    dupHead = newHead;
+    tracker = tracker->next;
+
+    custom(tracker, dupHead);
+}
+
+ListNode *reverseList(ListNode *head)
+{
+    if (head == nullptr)
+        return nullptr;
+
+    ListNode *dupHead = new ListNode(head->val);
+    ListNode *tracker = head->next;
+    custom(tracker, dupHead);
+
+    return dupHead;
+}
 
 int main()
 {
