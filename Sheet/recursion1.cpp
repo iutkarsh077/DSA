ef5fd1c93497f83ff74878a723ae75791e65908d
utkarsh <utkarshsingh132003@gmail.com>
2025-06-12 06:27:14 +0530

    ans Pow recursion

--- Sheet/recursion1.cpp
+++ Sheet/recursion1.cpp
@@ -8,6 +8,22 @@ double myPow(double x, int n)
     return ans;
 }
 
+ double ansPow(double x, int n){
+        if(n == 1) return x;
+        if(n == 0) return 1;
+
+        if(n % 2 == 0){
+            return ansPow(x, n / 2) * ansPow(x, n / 2);
+        }
+        else{
+            return x * ansPow(x, n / 2) * ansPow(x, n / 2);
+        }
+    }
+
+    double myPow(double x, int n) {
+        return ansPow(x, n);
+    }
+
 int main()
 {
     return 0;
