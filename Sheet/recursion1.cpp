69a05f9e4938916f9a0648dfe71147dcc2b64b13
utkarsh <utkarshsingh132003@gmail.com>
2025-06-13 08:52:12 +0530

    for negative n

--- Sheet/recursion1.cpp
+++ Sheet/recursion1.cpp
@@ -24,6 +24,30 @@ double myPow(double x, int n)
         return ansPow(x, n);
     }
 
+
+     double ansPowNeg(double x, int n) {
+        if (n == 1)
+            return x;
+        if (n == 0)
+            return 1;
+
+         double half = ansPowNeg(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+
+    double myPowNeg(double x, int n) {
+        long nn = n;
+        if (n < 0) {
+            nn = -1 * nn;
+            return 1.0 / ansPowNeg(x, nn);
+        }
+        return ansPowNeg(x, nn);
+    }
+
 int main()
 {
     return 0;
