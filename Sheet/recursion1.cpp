292d18c04d881da1cfb94ed6204575b0f8dfd1a3
utkarsh <utkarshsingh132003@gmail.com>
2025-06-15 07:57:00 +0530

    count good numbers

--- Sheet/recursion1.cpp
+++ Sheet/recursion1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include<math.h>
-#include<bits/stdc++.h>
+#include <math.h>
+#include <bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9 + 7;
 
 // double myPow(double x, int n)
 // {
@@ -24,7 +25,6 @@ using namespace std;
 //     double myPow(double x, int n) {
 //         return ansPow(x, n);
 //     }
-
 
 //      double ansPowNeg(double x, int n) {
 //         if (n == 1)
@@ -49,38 +49,75 @@ using namespace std;
 //         return ansPowNeg(x, nn);
 //     }
 
+int myAtoi(string s)
+{
+    int i = 0;
+    long long int ans = 0;
+    int sign = 1;
+    int n = s.size();
 
-int myAtoi(string s) {
-        int i = 0;
-        long long int ans = 0;
-        int sign = 1;
-        int n = s.size();
+    while (i < n && s[i] == ' ')
+        i++;
 
-        while(i < n && s[i] == ' ') i++;
-
-        if(i < n && (s[i] == '+' || s[i] == '-')){
-            if(s[i] == '-') sign = -1;
-            i++;
-        } 
-
-        while(i < n && isdigit(s[i])){
-            ans = (ans * 10) + (s[i] - '0');
-
-            if(sign * ans > INT_MAX) return INT_MAX;
-            if(sign * ans < INT_MIN) return INT_MIN;
-
-            i++;
-        }
-
-        return sign * ans;
+    if (i < n && (s[i] == '+' || s[i] == '-'))
+    {
+        if (s[i] == '-')
+            sign = -1;
+        i++;
     }
+
+    while (i < n && isdigit(s[i]))
+    {
+        ans = (ans * 10) + (s[i] - '0');
+
+        if (sign * ans > INT_MAX)
+            return INT_MAX;
+        if (sign * ans < INT_MIN)
+            return INT_MIN;
+
+        i++;
+    }
+
+    return sign * ans;
+}
+
+int countGoodNumbers(long long n)
+{
+    long long int start = 1;
+    string startStr = to_string(start);
+    long long int count = 1;
+    unordered_map<int, int> mp;
+    mp.insert({2, 1});
+    mp.insert({3, 1});
+    mp.insert({5, 1});
+    mp.insert({7, 1});
+
+    while(startStr.size() <= n){
+        int i = 0;
+        while(i < startStr.size()){
+            int val = startStr[i] - '0';
+
+            if(val % 2 == 0 && i % 2 == 0){
+                count++;
+                i++;
+            }
+            else if((mp.find(val) != mp.end()) && (mp.find(i) != mp.end())){
+                count++;
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        start = start + 1;
+        startStr = to_string(start);
+    }
+    return count % MOD;
+}
 
 int main()
 {
-    string a = " -08";
-    auto val = a[2] - '0';
-    if(val % 2 == 0){
-        cout << "This is a number";
-    }
+    long long int ans = countGoodNumbers(4);
+    cout << ans;
     return 0;
 }
