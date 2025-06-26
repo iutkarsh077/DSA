a5813f6f2158ce6228b6af53715f4006192aa910
utkarsh <utkarshsingh132003@gmail.com>
2025-06-26 08:45:14 +0530

    recursive

--- Sheet/recursion3.cpp
+++ Sheet/recursion3.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+#include<cmath>
 using namespace std;
 
-int main()
+void IterativeKthSymbol()
 {
     vector<vector<int>> result;
     int n = 3;
     int k = 4;
     result.push_back({0});
-    for(int i = 1; i < n; i++){
+    for (int i = 1; i < n; i++)
+    {
         vector<int> temp;
-        for(int j = 0; j < result[i - 1].size(); j++){
+        for (int j = 0; j < result[i - 1].size(); j++)
+        {
             int upperVal = result[i - 1][j];
-            if(upperVal == 0){
+            if (upperVal == 0)
+            {
                 temp.push_back(0);
                 temp.push_back(1);
             }
-            if(upperVal == 1){
+            if (upperVal == 1)
+            {
                 temp.push_back(1);
                 temp.push_back(0);
             }
@@ -24,13 +29,38 @@ int main()
         result.push_back(temp);
     }
 
-    for(int i = 0; i < result.size(); i++){
-        for(int j = 0; j < result[i].size(); j++){
-            if(n - 1 == i && k - 1 == j){
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            if (n - 1 == i && k - 1 == j)
+            {
                 cout << "The value is: " << result[i][j];
-                return 1;
+                return;
             }
         }
     }
+}
+
+int solve(int n, int k){
+    if(n == 1 || k == 1){
+        return 0;
+    }
+
+    int len = pow(2, n - 1);
+    int mid = len/2;
+
+    if(k <= mid){
+        return solve(n - 1, k);
+    }
+    else{
+        return !solve(n - 1, k - mid);
+    }
+}
+
+int main()
+{
+    int ans = solve(4, 5);
+    cout << ans;
     return 0;
 }
