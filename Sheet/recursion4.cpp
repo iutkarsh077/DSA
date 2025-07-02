19163e12e1d544721d57709ba1757ccbcaf9a9e1
utkarsh <utkarshsingh132003@gmail.com>
2025-07-02 08:45:27 +0530

    fibo

--- Sheet/recursion4.cpp
+++ Sheet/recursion4.cpp
@@ -9,8 +9,43 @@ int factorial(int n){
     return n * factorial(n - 1);
 }
 
+void reachSchool(int src, int dest){
+    if(src == dest){
+        cout << "Reached School" << endl;
+        return;
+    }
+
+    cout << "Covered distance: " << src << endl;
+
+    reachSchool(src + 1, dest);
+}
+
+void fibo(int n, int first, int second){
+    if(n == 2) return;
+
+    int sum = first + second;
+    cout << sum << " ";
+    first = second;
+    second = sum;
+
+    return fibo(n - 1, first, second);
+}
+
+void fiboIterative(int n){
+    int first = 0;
+    int second = 1;
+
+    for(int i = 3; i <= n; i++){
+        int sum = first + second;
+        cout << sum << " ";
+
+        first = second;
+        second = sum;
+    }
+}
+
 int main(){
-    int ans = factorial(10);
-    cout << ans;
+    cout << 0 << " " << 1 << " ";
+    fibo(8, 0, 1);
     return 0;
 }
