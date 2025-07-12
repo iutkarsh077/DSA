9e94ee7753c2cdd3c8165f68bf52c50b9225480a
utkarsh <utkarshsingh132003@gmail.com>
2025-07-12 06:57:53 +0530

    height of bt

--- binaryTrees/bt2.cpp
+++ binaryTrees/bt2.cpp
@@ -28,7 +28,6 @@ Node *BuiltTree(Node *&root)
     if (data == -1)
         return nullptr;
 
-
     cout << "Enter the data for left of " << data << " : ";
     root->left = BuiltTree(root->left);
     cout << "Enter the data for right of " << data << " : ";
@@ -37,8 +36,10 @@ Node *BuiltTree(Node *&root)
     return root;
 }
 
-void PostOrder(Node *&root){
-    if(root == nullptr){
+void PostOrder(Node *&root)
+{
+    if (root == nullptr)
+    {
         return;
     }
 
@@ -47,8 +48,10 @@ void PostOrder(Node *&root){
     cout << root->data << " ";
 }
 
-void PreOrder(Node *&root){
-    if(root == nullptr){
+void PreOrder(Node *&root)
+{
+    if (root == nullptr)
+    {
         return;
     }
 
@@ -57,8 +60,10 @@ void PreOrder(Node *&root){
     PreOrder(root->right);
 }
 
-void Inorder(Node *&root){
-    if(root == nullptr){
+void Inorder(Node *&root)
+{
+    if (root == nullptr)
+    {
         return;
     }
 
@@ -101,18 +106,62 @@ void LevelOrderTransversal(Node *root)
     }
 }
 
+void heightOfBT(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(nullptr);
+    int cnt = 0;
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == nullptr)
+        {
+            cnt++;
+            if (!q.empty())
+            {
+                q.push(nullptr);
+            }
+        }
+
+        else
+        {
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+
+    cout << "The height is: " << cnt << endl;
+}
+
 int main()
 {
     Node *root = nullptr;
 
     BuiltTree(root);
-    LevelOrderTransversal(root);
-    cout << endl << "In Order is: ";
-    Inorder(root);
-    cout << endl << "Post Order is: ";
-    PostOrder(root);
+    // LevelOrderTransversal(root);
+    // cout << endl
+    //      << "In Order is: ";
+    // Inorder(root);
+    // cout << endl
+    //      << "Post Order is: ";
+    // PostOrder(root);
 
-    cout << endl << "Pre Order is: ";
-    PreOrder(root);
+    // cout << endl
+    //      << "Pre Order is: ";
+    // PreOrder(root);
+
+    heightOfBT(root);
     return 0;
 }
+
+//1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
