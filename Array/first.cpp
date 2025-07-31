8ba0c09b07b8241a2c2636c55d228b1cf9495675
utkarsh <utkarshsingh132003@gmail.com>
2025-07-31 08:42:42 +0530

    sum

--- Array/first.cpp
+++ Array/first.cpp
@@ -157,7 +157,8 @@ int longestSubStr()
     cout << "Longest sub string is: " << maxLen << " " << ans;
 }
 
-int betterLongestStr(){
+int betterLongestStr()
+{
     string s = "cadbzabcd";
     unordered_map<int, int> mp;
     int n = s.size();
@@ -165,9 +166,12 @@ int betterLongestStr(){
     int r = 0;
     int maxLen = 0;
 
-    while(r < s.size()){
-        if(mp[s[r]] != -1){
-            if(mp[s[r]] >= l){
+    while (r < s.size())
+    {
+        if (mp[s[r]] != -1)
+        {
+            if (mp[s[r]] >= l)
+            {
                 l = mp[s[r]] + 1;
             }
         }
@@ -181,15 +185,18 @@ int betterLongestStr(){
     cout << "Max Len is: " << maxLen;
 }
 
-void maxConsecOnes(){
-    vector<int> nums = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+void maxConsecOnes()
+{
+    vector<int> nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
     int maxi = 0;
     int sum = 0;
 
-    for(int i = 0; i < nums.size(); i++){
+    for (int i = 0; i < nums.size(); i++)
+    {
         sum = sum + nums[i];
 
-        if(nums[i] == 0){
+        if (nums[i] == 0)
+        {
             sum = 0;
         }
 
@@ -199,8 +206,91 @@ void maxConsecOnes(){
     cout << "Max one is: " << maxi;
 }
 
+void maxConsecOnesTwo()
+{
+    int maxi = 0;
+    vector<int> nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int zeroes = 0;
+        for (int j = i; j < nums.size(); j++)
+        {
+            if (nums[j] == 0)
+            {
+                zeroes = zeroes + 1;
+            }
+
+            if (zeroes <= 2)
+            {
+                int len = j - i + 1;
+                maxi = max(len, maxi);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    cout << "Max ones is: " << maxi;
+}
+
+void slideWindowConecOnes()
+{
+    int maxi = 0;
+    vector<int> nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+    int l = 0;
+    int r = 0;
+    int zeroes = 0;
+    while(r < nums.size()){
+        if(nums[r] == 0){
+            zeroes++;
+        }
+
+        if(zeroes > 2){
+            while(zeroes > 2){
+                if(nums[l] == 0){
+                    zeroes--;
+                }
+                l++;
+            }
+        }
+
+        maxi = max(maxi, r - l + 1);
+        r++;
+    }
+}
+
+void sum(){
+     int arr[] = {2, 0, 0, 3, -1, -1};
+    map<long long, int> mp;
+    long long sum = 0;
+    int maxi = 0;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = -2;
+    for(int i = 0; i < n; i++){
+        sum = sum + arr[i];
+
+        if(sum == k){
+            maxi = max(maxi, i + 1);
+        }
+
+        long long rem = sum - k;
+        if(mp.find(rem) != mp.end()){
+            int len = i - mp[rem];
+            maxi = max(maxi, len);
+        }
+
+        if(mp.find(sum) == mp.end()){
+            mp[sum] = i;
+        }
+
+    }
+    cout << "The longest subarray is: " << maxi;
+}
+
 int main()
 {
-    maxConsecOnes();
+    maxConsecOnesTwo();
     return 0;
 }
