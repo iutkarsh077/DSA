80ffd3348229875bbb7a2518d402b326b1b65f59
utkarsh <utkarshsingh132003@gmail.com>
2025-08-01 07:15:23 +0530

    better solution

--- Array/first.cpp
+++ Array/first.cpp
@@ -306,6 +306,24 @@ void sum(){
         return result;
     }
 
+
+vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> result;
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++){
+          mp[nums[i]]++;
+        }
+
+        for(auto i = mp.begin(); i != mp.end(); i++){
+            if(i->second >= 2){
+                result.push_back(i->first);
+            }
+        }
+
+        return result;
+    }
+
 int main()
 {
     maxConsecOnesTwo();
