da1282dfcb4ef042b2a98ad3cd8f184bd5ca738b
utkarsh <utkarshsingh132003@gmail.com>
2025-08-07 08:51:19 +0530

    search in rotated

--- Array/first.cpp
+++ Array/first.cpp
@@ -339,6 +339,37 @@ vector<int> findDuplicates(vector<int> &nums)
     return result;
 }
 
+int search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() - 1;
+
+        while(s <= e){
+            int mid = s + ((e - s)/2);
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[s] <= nums[mid]){
+                if(nums[mid] >= target && nums[s] <= target){
+                    e = mid - 1;
+                }
+                else{
+                    s = mid + 1;
+                }
+            }
+
+            if(nums[mid] < nums[e]){
+                if(nums[mid] <= target && nums[e] >= target){
+                    s = mid + 1;
+                }
+                else{
+                     e = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
  int findMin(vector<int>& nums) {
         int mini = nums[0];
 
