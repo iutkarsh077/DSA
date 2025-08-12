c590470e27e285e5d1a80ad34faefe8e17cb4761
utkarsh <utkarshsingh132003@gmail.com>
2025-08-12 09:30:29 +0530

    pyramid stars

--- Array/patterns.cpp
+++ Array/patterns.cpp
@@ -13,19 +13,69 @@ void squareBracket()
     }
 }
 
-void upperTriangle(){
-    for(int i = 0; i < 6; i++){
-        for(int j = 0; j <= i; j++){
+void upperTriangle()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             cout << '*' << " ";
         }
         cout << endl;
     }
 }
 
-void printNumberOfJ(){
-    for(int i = 0; i < 6; i++){
-        for(int j = 0; j <= i; j++){
+void printNumberOfJ()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void printNumberOfI()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << i + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void ReverseStars()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 6; j > i; j--)
+        {
+            cout << 6 - j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pyramidStars()
+{
+    int n = 50;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= (n - 1) + i; j++)
+        {
+            if (i + j >= n - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
         }
         cout << endl;
     }
@@ -33,6 +83,6 @@ void printNumberOfJ(){
 
 int main()
 {
-    printNumberOfJ();
+    pyramidStars();
     return 0;
 }
