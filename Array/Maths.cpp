d27c1433135b5014493c88ac9bc5c86962ac1d72
utkarsh <utkarshsingh132003@gmail.com>
2025-08-18 07:40:48 +0530

    today maths

--- Array/Maths.cpp
+++ Array/Maths.cpp
@@ -1,24 +1,28 @@
-#include<iostream>
-#include<cstdlib>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
 
-void countDigits(){
+void countDigits()
+{
     long long n = 8726786278;
     int logResult = (int)(log10(n) + 1);
     int count = 0;
-    while(n > 0){
+    while (n > 0)
+    {
         n = n / 10;
         count = count + 1;
     }
     cout << "The number of digits are: " << count;
 }
 
-void reverseNumber(){
+void reverseNumber()
+{
     long long int n = 1534236469;
     long long int result = 0;
 
-    while(n > 0){
+    while (n > 0)
+    {
         int digit = n % 10;
         result = (result * 10) + digit;
         n = n / 10;
@@ -27,7 +31,74 @@ void reverseNumber(){
     cout << "The result is: " << result;
 }
 
-int main(){
-    reverseNumber();
+bool isPalindrome(int x)
+{
+    if (x < 0)
+        return false;
+
+    long long int val = 0;
+    int dupX = x;
+    while (dupX > 0)
+    {
+        int digit = dupX % 10;
+        val = (val * 10) + digit;
+        dupX = dupX / 10;
+    }
+
+    if (val == x)
+        return true;
+
+    return false;
+}
+
+int countNumberLength(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+int powerNumber(int n, int power)
+{
+    if (power <= 0)
+        return 1;
+
+    if (power % 2 == 0)
+    {
+        return powerNumber(n, power / 2) * powerNumber(n, power / 2);
+    }
+    else
+    {
+        return n * powerNumber(n, power / 2) * powerNumber(n, power / 2);
+    }
+}
+
+void armStrong(){
+    int n = 152;
+    int dupN = n;
+    int ans = 0;
+    int count = countNumberLength(n);
+    while(n > 0){
+        int digit = n % 10;
+        ans += powerNumber(digit, count);
+        n = n /10;
+    }
+    cout << "Answer is: " << ans;
+
+    if(ans == dupN){
+        cout << endl << "Number is Palindrome";
+    }
+    else{
+        cout << endl << "No, its not a Palindrome";
+    }
+}
+
+int main()
+{
+    armStrong();
     return 0;
 }
