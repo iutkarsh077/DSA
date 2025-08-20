a43ec528ba9117f0c43f9d837e80ddd0fccd4086
utkarsh <utkarshsingh132003@gmail.com>
2025-08-20 07:55:33 +0530

    recursion basics

--- Array/Maths.cpp
+++ Array/Maths.cpp
@@ -124,9 +124,26 @@ int GCDBetter(int big, int small){
     return GCDBetter(myBig, mySmall1);
 }
 
+void printAlldivisor(int n){
+    vector<int> ans;
+
+    for(int i = 1; i <= sqrt(n); i++){
+        if(n % i == 0){
+            ans.push_back(i);
+            if(n / i != i){
+                ans.push_back(n/i);
+            }
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+}
+
 int main()
 {
-    int ans= GCDBetter(13, 0);
-    cout << ans;
+    printAlldivisor(36);
     return 0;
 }
---
+++ Array/recursion.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void printName(int n){
+    if(n == 0) return;
+
+    cout << "Utkarsh" << " ";
+    printName(n - 1);
+}
+
+void printNumber(int n){
+    if(n == 0) return;
+
+    printNumber(n - 1);
+    cout << n << " ";
+}
+ 
+int sumOfNumbers(int n){
+    if(n == 0) return 0;
+
+    return n + sumOfNumbers(n - 1);
+}
+
+int factorial(int n){
+    if(n == 0) return 1;
+
+    return n * factorial(n - 1);
+}
+
+void reverse(int i, int j, vector<int> &nums){
+    if(i >= j) return;
+
+    swap(nums[i], nums[j]);
+
+    reverse(i + 1, j - 1, nums);
+}
+
+bool palindrome(string val, int i, int j){
+    if(i >= j) return true;
+
+    if(val[i] != val[j]){
+        return false;
+    } 
+
+    return palindrome(val, i + 1, j - 1);
+}
+
+int fibo(int n){
+    if(n <= 1)  return n;
+
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main(){
+
+    cout << fibo(5);
+    return 0;
+}
