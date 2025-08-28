9e87d0c72cc0b1f20cb6c3c08be84ea6e80ab28d
utkarsh <utkarshsingh132003@gmail.com>
2025-08-28 07:51:59 +0530

    Rotate

--- Array/arr1.cpp
+++ Array/arr1.cpp
@@ -1,28 +1,32 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-void largestElement(){
+void largestElement()
+{
     vector<int> nums;
     nums = {3, 1, 5, 4, 2, 7, 81, 5, 9, 5, 0};
     int maxi = INT_MIN;
 
-    for(int i = 0; i < nums.size(); i++){
+    for (int i = 0; i < nums.size(); i++)
+    {
         maxi = max(maxi, nums[i]);
     }
 
     cout << "Largest element is: " << maxi;
 }
 
-void secondLargestElement(){
+void secondLargestElement()
+{
     vector<int> nums;
     nums = {3, 1, 5, 4, 2, 7, 81, 5, 9, 5, 0};
     int maxi = INT_MIN;
     int maxiIndex = 0;
 
-     for(int i = 0; i < nums.size(); i++){
-        if(nums[i] >= maxi){
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] >= maxi)
+        {
             maxi = nums[i];
             maxiIndex = i;
         }
@@ -31,32 +35,65 @@ void secondLargestElement(){
     nums[maxiIndex] = INT_MIN;
     int secMaxi = INT_MIN;
 
-    for(int i = 0; i < nums.size(); i++){
+    for (int i = 0; i < nums.size(); i++)
+    {
         secMaxi = max(secMaxi, nums[i]);
     }
 
     cout << "second largest element is: " << secMaxi;
 }
 
-bool check(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return true;
+bool check(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+        return true;
 
-        int cnt = 0;
-        if(nums[0] < nums[n - 1]) cnt++;
+    int cnt = 0;
+    if (nums[0] < nums[n - 1])
+        cnt++;
 
-        for(int i = 0; i < nums.size() - 1; i++){
-            if(nums[i] > nums[i + 1]){
-                cnt++;
-            }
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        if (nums[i] > nums[i + 1])
+        {
+            cnt++;
         }
-
-        if(cnt <= 1) return true;
-
-        return false;
     }
 
-int main(){
-    secondLargestElement();
+    if (cnt <= 1)
+        return true;
+
+    return false;
+}
+
+void rotate(vector<int> &nums, int k)
+{
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
+
+void rotaeByOnePlace(){
+    vector<int> nums;
+    nums = {1, 2, 3, 4, 5};
+
+    int first = nums[0];
+    if(nums.size() == 1) return;
+
+    for(int i = 1; i < nums.size(); i++){
+        nums[i - 1] = nums[i];
+    }
+
+    nums[nums.size() - 1] = first;
+    for(int i = 0; i < nums.size(); i++){
+        cout << nums[i] << " ";
+    }
+}
+
+int main()
+{
+    rotaeByOnePlace();
     return 0;
 }
