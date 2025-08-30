5857bf060213222383d8bc7635920bac8aa78b37
utkarsh <utkarshsingh132003@gmail.com>
2025-08-30 08:19:30 +0530

    single number

--- Array/arr1.cpp
+++ Array/arr1.cpp
@@ -184,6 +184,25 @@ void FindTheUnion()
     }
 }
 
+int singleNumber(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        sort(nums.begin(), nums.end());
+
+        if(nums[0] != nums[1]) return nums[0];
+
+        for(int i = 1; i < nums.size() - 1; i++){
+            if(nums[i] != nums[i - 1] && nums[i] != nums[i + 1]){
+                return nums[i];
+            }
+        }
+
+        if(nums[nums.size() - 1] != nums[nums.size() - 2]){
+            return nums[nums.size() - 1];
+        }
+
+        return -1;
+    }
+
 int main()
 {
     FindTheUnion();
