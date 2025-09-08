9556cc75bf4bfde1d89f97c902968e7a2c9f86aa
utkarsh <utkarshsingh132003@gmail.com>
2025-09-08 07:35:14 +0530

    rotate 90

--- Array/arr1.cpp
+++ Array/arr1.cpp
@@ -461,6 +461,25 @@ void setZeroes()
     }
 }
 
+void rotate(vector<vector<int>> &matrix)
+{
+
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = i; j < matrix[i].size(); j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+
+    return;
+}
+
 int main()
 {
     setZeroes();
