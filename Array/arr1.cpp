30506b4370ccab66cab38ccf7c8acd0a9813d987
utkarsh <utkarshsingh132003@gmail.com>
2025-09-17 08:58:44 +0530

    subArrayZero

--- Array/arr1.cpp
+++ Array/arr1.cpp
@@ -606,8 +606,53 @@ void sprialMatrix()
     }
 }
 
+vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    ans.insert({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                }
+
+                else if (sum > 0) {
+                    right--;
+                }
+
+                else {
+                    left++;
+                }
+            }
+        }
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+
+
+    void subArrayZero(){
+    vector<int> nums = {6, -2, 2, -8, 1, 7, 4, -10};
+    unordered_map<int, int> mp;
+    int maxi = 0;
+    for(int i = 0; i < nums.size(); i++){
+        int sum = 0;
+        for(int j = i; j < nums.size(); j++){
+            sum = sum + nums[j];
+            if(sum == 0){
+                maxi = max(maxi, j - i + 1);
+            }
+        }
+    }
+
+    cout << "Maximum subarray is: " << maxi;
+}
+
 int main()
 {
-    sprialMatrix();
+    subArrayZero();
     return 0;
 }
