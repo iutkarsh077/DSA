fd3fd5e011039171cc571b10fb70d53a8f497bf2
utkarsh <utkarshsingh132003@gmail.com>
2025-09-28 08:10:22 +0530

    searchRange

--- Array/arr1.cpp
+++ Array/arr1.cpp
@@ -944,6 +944,89 @@ int searchInsert(vector<int>& nums, int target) {
         return index;
     }
 
+bool search3(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() - 1;
+
+        while(s <= e){
+            int mid = s +((e - s)/2);
+
+            if(nums[mid] == target){
+                return true;
+            }
+
+            if (nums[s] == nums[mid] && nums[mid] == nums[e]) {
+                s++;
+                e--;
+                continue;
+            }
+
+            if(nums[s] <= nums[mid]){
+                 if(nums[s] <= target && nums[mid] >= target){
+                    e = mid - 1;
+                }
+                else{
+                    s = mid + 1;
+                }
+            }
+
+            if(nums[mid] <= nums[e]){
+                 if(nums[mid] <= target && nums[e] >= target){
+                    s = mid + 1;
+                }
+                else{
+                    e = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int s = 0;
+        int e = n - 1;
+        int ans1 = -1;
+        int ans2 = -1;
+
+        while(s <= e){
+            int mid = s + ((e - s)/2);
+
+            if(nums[mid] == target){
+                ans1 = mid;
+                e = mid - 1;
+            }
+            else if(nums[mid] < target){
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
+        }
+
+
+        s = 0;
+        e = n - 1;
+
+        while(s <= e){
+            int mid = s + ((e - s)/2);
+
+            if(nums[mid] == target){
+                ans2 = mid;
+                s = mid + 1;
+            }
+            else if(nums[mid] < target){
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
+        }
+
+        return {ans1, ans2};
+    }
+
 int main()
 {
     vector<int> nums = {1, 3, 5};
