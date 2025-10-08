1a650b155baf64589b55c1d79c5949471e575973
utkarsh <utkarshsingh132003@gmail.com>
2025-10-08 08:57:17 +0530

    largestOddNumber

--- Array/arr1.cpp
+++ Array/arr1.cpp
@@ -1472,6 +1472,21 @@ string reverseWords(string s)
     return result;
 }
 
+ string largestOddNumber(string num) {
+        string ans = "";
+
+        for(int i = num.size() - 1; i >= 0; i--){
+            int digit = num[i] - '0';
+
+            if(digit % 2 != 0){
+                ans = num.substr(0, i  + 1);
+                break;
+            }
+        }
+
+        return ans;
+    }
+
 int main()
 {
     removeOuterParentheses();
