dd3ce7ef17a97da9236c275abfe02b224c33621d
utkarsh <utkarshsingh132003@gmail.com>
2025-12-20 08:23:50 +0530

    sorting

--- data_structure/sorting2.cpp
+++ data_structure/sorting2.cpp
@@ -1,26 +1,125 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void print(vector<int> arr){
-    for(int i = 0; i < arr.size(); i++){
+void print(vector<int> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
         cout << arr[i] << " ";
     }
 }
 
-void bubbleSort(vector<int> &arr){
-    for(int i = 0; i < arr.size(); i++){
-        for(int j = i; j < arr.size() - i; j++){
-            if(arr[j] > arr[j + 1]){
+void bubbleSort(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr.size() - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
-int main(){
-    vector<int> arr = {5, 1, 6, 2, 8, 4};
-    bubbleSort(arr);
+void selectionSort(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int minIndex = i;
+        bool swapped = false;
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[minIndex] > arr[j])
+            {
+                swapped = true;
+                minIndex = j;
+            }
+        }
+        if (swapped)
+        {
+            swap(arr[minIndex], arr[i]);
+        }
+    }
+}
+
+void InsertionSort(vector<int> &arr)
+{
+    for (int i = 1; i < arr.size(); i++)
+    {
+        int j = i - 1;
+        int temp = arr[i];
+        for (; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void MergeSort(vector<int> &arr, int low, int mid, int high)
+{
+    int left = low;
+    int right = mid + 1;
+
+    vector<int> temp;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i = low; i <= high; i++){
+        arr[i] = temp[i - low];
+    }
+}
+
+void divide(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+
+    int mid = low + ((high - low) / 2);
+
+    divide(arr, low, mid);
+    divide(arr, mid + 1, high);
+    MergeSort(arr, low, mid, high);
+}
+
+int main()
+{
+    vector<int> arr = {5, 1, 6, 2, 8, 4, 4, 1, 1, 8};
+    divide(arr, 0, arr.size() - 1);
     print(arr);
     return 0;
 }
