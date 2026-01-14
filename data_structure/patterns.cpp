810f3ac1542b98e397b9d279b31957768e4ab88a
utkarsh <utkarshsingh132003@gmail.com>
2026-01-14 19:27:42 +0530

    patterns2

--- data_structure/patterns.cpp
+++ data_structure/patterns.cpp
@@ -156,12 +156,16 @@ void DiamondStar()
     }
 }
 
-void halfDiamondStar(){
+void halfDiamondStar()
+{
     int n = 5;
 
-    for(int i = 0; i < 2 * n - 1; i++){
-        for(int j = 0; j <= i; j++){
-            if(i + j <= 2 * n - 2){
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (i + j <= 2 * n - 2)
+            {
                 cout << "*";
             }
         }
@@ -170,8 +174,246 @@ void halfDiamondStar(){
     }
 }
 
+void NumberRightTriangle()
+{
+    int n = 8;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << 1 << " ";
+            }
+            else
+            {
+                cout << 0 << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void TombStructure()
+{
+    int n = 7;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j + 1;
+        }
+
+        for (int j = i + 1; j < (2 * n) - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = (2 * n) - i - 1; j <= 2 * n - 1; j++)
+        {
+            cout << 2 * n - j;
+        }
+
+        cout << endl;
+    }
+}
+
+void RightTriangleNumber2()
+{
+    int n = 10;
+    int count = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+}
+
+void RightTriangleAlphabet()
+{
+    int n = 10;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << char(65 + j);
+        }
+        cout << endl;
+    }
+}
+
+void ReverseRightTriangleAlphabet()
+{
+    int n = 10;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << char(65 + j);
+        }
+
+        cout << endl;
+    }
+}
+
+void aba()
+{
+    int n = 8;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << char(65 + j);
+        }
+
+        for (int j = i - 1; j >= 0; j--)
+        {
+            cout << char(65 + j);
+        }
+
+        cout << endl;
+    }
+}
+
+void EAlphabet()
+{
+    int n = 10;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << char(((65 + n - 1) - i) + j);
+        }
+
+        cout << endl;
+    }
+}
+
+void VerticalDiamond()
+{
+    int n = 10;
+    // Upper half
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < i * 2; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower Half
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < (n * 2) - (i * 2) - 2; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+
+void ButterFly()
+{
+    int n = 10;
+    // Upper half
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < (n * 2) - (i * 2) - 2; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower Half
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < i * 2; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+
+void HollowRectangle(){
+    int n = 100;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == 0 || j == 0 || j == n - 1 || i == n - 1){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    halfDiamondStar();
+    HollowRectangle();
     return 0;
 }
