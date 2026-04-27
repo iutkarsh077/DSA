2cc9cfaa3a7eb4b95e4239866378b355ec17911e
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-04-27 06:53:27 +0530

    Triangle pattern

--- april2026/starproblems.cpp
+++ april2026/starproblems.cpp
@@ -1,26 +1,167 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void RectangleStar(int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+void RectangleStar(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cout << '*';
         }
         cout << endl;
     }
 }
 
-
-void Triangle1(int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j <= i; j++){
+void Triangle1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             cout << '*';
         }
         cout << endl;
     }
 }
 
-int main(){
-    Triangle1(10);
+void Triangle2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Triangle3(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << i + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Triangle4(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+}
+
+void Triangle5(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << j + 1 << ' ';
+        }
+        cout << endl;
+    }
+}
+
+void Triangle6(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n + i; j++)
+        {
+            if (i + j >= n - 1)
+            {
+                cout << '*';
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+}
+
+void Triangle7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < ((n * 2) - i - 1); j++)
+        {
+            if (j < i)
+            {
+                cout << ' ';
+            }
+            else
+            {
+                cout << '*';
+            }
+        }
+        cout << endl;
+    }
+}
+
+void Diamond(int n)
+{
+    Triangle6(n);
+    Triangle7(n);
+}
+
+void Triangle8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+}
+
+void Triangle9(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        bool isEven = i % 2 == 0 ? true : false;
+        for (int j = 0; j <= i; j++)
+        {
+            if (isEven)
+            {
+                cout << '1';
+            }
+            else
+            {
+                cout << '0';
+            }
+            isEven = !isEven;
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    Triangle9(10);
     return 0;
 }
