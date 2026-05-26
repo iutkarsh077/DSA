396c5c2fe8b8379af27932f01b98b7b707427118
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-05-26 20:48:54 +0530

    binary search

--- april2026/Basics.cpp
+++ april2026/Basics.cpp
@@ -320,6 +320,55 @@ vector<vector<int>> generate(int n)
     return nums;
 }
 
+int search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int s = 0;
+    int e = n - 1;
+
+    while (s <= e)
+    {
+        int mid = s + ((e - s) / 2);
+
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] > target)
+            e = mid - 1;
+        else
+        {
+            s = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+int lowerBound(vector<int> &arr, int target)
+{
+    int n = arr.size();
+    int s = 0;
+    int e = n - 1;
+
+    int ans = n;
+
+    while (s <= e)
+    {
+        int mid = s + ((e - s) / 2);
+
+        if (arr[mid] >= target)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
     // NToOne(10);
