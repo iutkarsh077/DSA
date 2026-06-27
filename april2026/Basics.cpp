9c40bdac7c7c38b7c26eeb7b6e77b6ae3e27b7da
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-06-27 09:03:15 +0530

    pow

--- april2026/Basics.cpp
+++ april2026/Basics.cpp
@@ -412,6 +412,37 @@ int myAtoi(string nums)
     return val;
 }
 
+double recur(double x, long n)
+{
+    if (n == 0)
+        return 1;
+    if (n == 1)
+        return x;
+
+    double half = recur(x, n / 2);
+    if (n % 2 == 0)
+    {
+        return half * half;
+    }
+    else
+    {
+        return x * half * half;
+    }
+}
+double myPow(double x, int n)
+{
+    long dupN = n;
+
+    if (n < 0)
+    {
+        dupN = dupN * -1;
+        double ans = recur(x, dupN);
+        return 1.0 / ans;
+    }
+
+    return recur(x, dupN);
+}
+
 int main()
 {
     // NToOne(10);
