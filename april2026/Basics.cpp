94b1c77afab8e2f42be9f1174e4b5976b8a03cc1
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-07-13 06:10:15 +0530

    merge

--- april2026/Basics.cpp
+++ april2026/Basics.cpp
@@ -21,6 +21,16 @@ void CountNumber(int n)
     cout << "The number " << n << " has " << cnt << " numbers";
 }
 
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = 0; i < n; i++){
+            nums1[m + i] = nums2[i];
+        }
+
+        sort(nums1.begin(), nums1.end());
+        return;
+    }
+
 int reverse(int x)
 {
     int ans = 0;
