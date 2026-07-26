76a285f663848d561b1636a64c8541781c77a25a
utkarshaws7-sudo <utkarsh.aws7@gmail.com>
2026-07-26 08:55:17 +0530

    merge array

--- Sheet/sort2.cpp
+++ Sheet/sort2.cpp
@@ -45,6 +45,50 @@ void DivideArray(vector<int> &arr, int low, int high){
     MergeArray(arr, low, mid, high);
 }
 
+void MergeArray22(vector<int> &arr, int low, int mid, int high){
+    int left = low;
+    int right = mid + 1;
+
+    vector<int> temp;
+    while(left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+
+    for(int i = low; i <= high; i++){
+        arr[i] = temp[i - low];
+    }
+}
+
+void DivideArray(vector<int> &arr, int low, int high){
+    if(low >= high){
+        return;
+    }
+
+    int mid = low + ((high - low))/2;
+    DivideArray(arr, low, mid);
+    DivideArray(arr, mid + 1, high);
+    MergeArray(arr, low, mid, high);
+}
+
+
 int main(){
     vector<int> arr = {1};
     DivideArray(arr, 0, arr.size() - 1);
