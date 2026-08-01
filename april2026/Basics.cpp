a21fdd5793454127cc5ef6d159bca337908e8190
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-01 09:29:49 +0530

    remove dups

--- april2026/Basics.cpp
+++ april2026/Basics.cpp
@@ -640,6 +640,39 @@ int lengthOfLongestSubstring(string s)
     return maxi;
 }
 
+int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+
+        while(i < nums.size() - 1){
+            if(nums[i] == nums[i + 1]){
+                nums.erase(nums.begin() + i, nums.begin() + i + 1);
+            }
+            else{
+                i++;
+            }
+        }
+
+        return nums.size();
+}
+
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi = 0;
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 1){
+                maxi = max(maxi, cnt);
+                cnt = 0;
+            }
+            else{
+                cnt++;
+            }
+        }
+
+        maxi = max(cnt, maxi);
+
+        return maxi;
+}
+
 vector<vector<int>> PascalTriangle(int numRows)
 {
     vector<vector<int>> nums;
