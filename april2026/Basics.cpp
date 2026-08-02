f4a3c3196a1c08990c2f2867e27a84bfe13c8c89
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-02 07:56:28 +0530

    singleNonDuplicate

--- april2026/Basics.cpp
+++ april2026/Basics.cpp
@@ -655,6 +655,44 @@ int removeDuplicates(vector<int>& nums) {
         return nums.size();
 }
 
+int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        int start = 1;
+        int end = n - 2;
+
+        if(nums[0] != nums[1]) return nums[0];
+
+        if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        while(start <= end){
+            int mid = start + ((end - start)/2);
+
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
+                return nums[mid];
+            }
+
+            else if(nums[mid] != nums[mid - 1]){
+                if(mid % 2 != 0){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+
+            else{
+                if(mid % 2 != 0){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+}
+
 int findMaxConsecutiveOnes(vector<int>& nums) {
         int maxi = 0;
         int cnt = 0;
