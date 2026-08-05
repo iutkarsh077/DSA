f7033ab719bbc609ae753e17abc24aa3e347bac5
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-05 08:28:33 +0530

    stars

--- april2026/august2026/patterns.cpp
+++ april2026/august2026/patterns.cpp
@@ -1,18 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void patterns(){
-    int n = 5;
+void patterns()
+{
+    int n = 7;
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n - i; j++){
-            cout << j + 1 << " ";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " " << " ";
         }
+
+        int starsCount = (2 * (n - i)) - 1;
+        for (int j = 0; j < starsCount; j++)
+        {
+            cout << "*" << " ";
+        }
+
         cout << endl;
     }
 }
 
-int main(){
+int main()
+{
     patterns();
     return 0;
 }
