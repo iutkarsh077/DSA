03809700290cd9ff908241619d7c66bbea21214c
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-09 08:01:40 +0530

    patterns

--- april2026/august2026/patterns.cpp
+++ april2026/august2026/patterns.cpp
@@ -3,15 +3,28 @@ using namespace std;
 
 void patterns()
 {
-    int n = 5;
-    int val = 65;
+    int n = 4;
+
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < i + 1; j++){
-            cout << char(val) << " ";
+        for(int j = 0; j < n - i - 1; j++){
+            cout << "_" << " ";
         }
-        val++;
+
+        int val = 65;
+        for(int j = 0; j <= i; j++){
+            cout << char(val) << " ";
+            val++;
+        }
+
+        val = val - 2;
+        for(int j = 0; j < i; j++){
+            cout << char(val) << " ";
+            val--;
+        }
+
         cout << endl;
     }
+    
 }
 
 int main()
