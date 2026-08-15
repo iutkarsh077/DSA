acc745d82651df76b9ebf738485458945fa74bf1
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-15 22:54:13 +0530

    squares

--- april2026/august2026/patterns.cpp
+++ april2026/august2026/patterns.cpp
@@ -3,45 +3,19 @@ using namespace std;
 
 void patterns()
 {
-    int n = 5;
+    int n = 4;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << "*" << " ";
-        }
-
-        for (int j = 0; j < i * 2; j++)
-        {
-            cout << "_" << " ";
-        }
-
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << "*" << " ";
-        }
-
-        cout << endl;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        int j = 0;
-        for (; j <= i; j++)
-        {
-            cout << "*" << " ";
-            
-        }
-
-        for (; j < (n * 2) - i - 1; j++)
-        {
-            cout << "_" << " ";
-         
-        }
-
-        for(; j < (n * 2); j++){
-            cout << "*" << " ";
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == 0 || i == n - 1){
+                cout << "*";
+            }
+            else if(j == 0 || j == n - 1){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
         }
         cout << endl;
     }
