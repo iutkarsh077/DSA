eadf52fa935ea88e6fcd01489e48d68e8e27ae49
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-22 17:49:07 +0530

    sorting

--- april2026/august2026/maths.cpp
+++ april2026/august2026/maths.cpp
@@ -66,6 +66,36 @@ int gcd(int a, int b)
     return a;
 }
 
+vector<int> getDivisors(int n)
+{
+    vector<int> ans;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            ans.push_back(i);
+        }
+    }
+
+    return ans;
+}
+
+bool isPrime(int n)
+{
+
+    if (n == 1)
+        return false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 bool armstrongNumber(int n)
 {
     // code here
@@ -88,7 +118,173 @@ bool armstrongNumber(int n)
     return val == n ? true : false;
 }
 
+void RecurNTimes(int n)
+{
+    if (n == 0)
+        return;
+
+    cout << "Something" << endl;
+
+    RecurNTimes(n - 1);
+}
+
+void OnetoN(int n, int i)
+{
+    if (i > n)
+        return;
+
+    cout << i << endl;
+
+    OnetoN(n, i + 1);
+}
+
+int Facto(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+
+    return n * Facto(n - 1);
+}
+
+void RevArray(vector<int> &arr, int i)
+{
+    int n = arr.size();
+    if (i == n - i - 1)
+        return;
+
+    swap(arr[i], arr[n - i - 1]);
+
+    RevArray(arr, i + 1);
+}
+
+bool isPalindrome(string s)
+{
+    string value = "";
+
+    int i = 0;
+    while (i < s.size())
+    {
+        if (isalnum(s[i]))
+        {
+            value += tolower(s[i]);
+        }
+        i++;
+    }
+
+    string dupVal = "";
+
+    for (int i = value.size() - 1; i >= 0; i--)
+    {
+        dupVal += value[i];
+    }
+
+    if (dupVal == value)
+        return true;
+
+    return false;
+}
+
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+unordered_map<int, int> FrequencyCount(vector<int> arr)
+{
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        mp[arr[i]]++;
+    }
+
+    return mp;
+}
+
+void selectionSort()
+{
+    vector<int> arr = {4, 1, 2, 6, 7, 5, 8, 8};
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int mini = i;
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[mini] > arr[j])
+            {
+                mini = j;
+            }
+        }
+        swap(arr[i], arr[mini]);
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void insertionSort()
+{
+    vector<int> arr = {5, 4, 1, 6, 7, 8, 9, 3, 2, 3};
+
+    int i = 0;
+    int n = arr.size();
+
+    while (i < n)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+
+        while (j >= 0)
+        {
+            if (temp < arr[j])
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+            j--;
+        }
+
+        arr[j + 1] = temp;
+        i++;
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void bubbleSort()
+{
+    vector<int> arr = {4, 1, 2, 6, 7, 5, 8, 8};
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr.size() - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
 int main()
 {
+    insertionSort();
     return 0;
 }
