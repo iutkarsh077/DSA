ad114cbadb611b5735eac667f8f4c14cc9ab5c03
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-23 16:15:25 +0530

    sorting

--- april2026/august2026/stl.cpp
+++ april2026/august2026/stl.cpp
@@ -1,56 +1,122 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void pairme(){
+void pairme()
+{
     pair<int, pair<int, int>> p = {1, {2, 3}};
-
 
     cout << p.second.second;
 }
 
-bool comp(pair<int, int> p1, pair<int, int> p2){
-    if(p1.second > p2.second) return false;
-    if(p1.second < p2.second) return true;
+bool comp(pair<int, int> p1, pair<int, int> p2)
+{
+    if (p1.second > p2.second)
+        return false;
+    if (p1.second < p2.second)
+        return true;
 
-    if(p1.first > p2.first) return true;
+    if (p1.first > p2.first)
+        return true;
     return false;
 }
 
-void myset(){
+void myset()
+{
     set<int, greater<int>> s1 = {4, 1, 3, 2, 5, 6};
     s1.insert(20);
     s1.erase(1);
 
-    for(auto i = s1.begin(); i != s1.end(); i++){
+    for (auto i = s1.begin(); i != s1.end(); i++)
+    {
         cout << *(i) << " ";
     }
 
     cout << endl;
     cout << s1.size() << endl;
 
-    auto it =  s1.find(90);
+    auto it = s1.find(90);
 
     cout << *it;
-
 
     pair<int, int> mp[] = {{2, 3}, {4, 5}, {3, 2}};
 
     sort(mp, mp + 3, comp);
 }
 
-void iter(){
+void iter()
+{
     vector<int> v = {10, 20, 30, 40};
 
     v.insert(v.begin(), {1, 2, 3});
 
-    for(auto i = v.begin(); i != v.end(); i++){
+    for (auto i = v.begin(); i != v.end(); i++)
+    {
         cout << *(i) << " ";
     }
 }
 
+void MergeArray(vector<int> &arr, int start, int mid, int end)
+{
+    vector<int> temp;
 
-int main(){
-    // pairme();
-    myset();
+    int left = start;
+    int right = mid + 1;
+
+    while (left <= mid && right <= end)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= end)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = start; i <= end; i++)
+    {
+        arr[i] = temp[i - start];
+    }
+}
+
+void printMe(vector<int> arr)
+{
+    for(int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+}
+
+void DivideMe(vector<int> &arr, int start, int end)
+{
+    if (start >= end)
+        return;
+
+    int mid = start + (end - start) / 2;
+
+    DivideMe(arr, start, mid);
+    DivideMe(arr, mid + 1, end);
+    MergeArray(arr, start, mid, end);
+}
+
+int main()
+{
+    vector<int> arr = {5, 6, 2, 1, 9, 8, 6, 4};
+    DivideMe(arr, 0, arr.size() - 1);
+    printMe(arr);
     return 0;
 }
