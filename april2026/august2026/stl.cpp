bfea5283138ad86a2333b0901c434d42d65d44db
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-09-11 07:59:58 +0530

    max product

--- april2026/august2026/stl.cpp
+++ april2026/august2026/stl.cpp
@@ -94,6 +94,28 @@ void MergeArray(vector<int> &arr, int start, int mid, int end)
     }
 }
 
+int maxProduct(vector<int> &nums)
+{
+    int n = nums.size();
+    int pre = 1;
+    int suf = 1;
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (pre == 0)
+            pre = 1;
+        if (suf == 0)
+            suf = 1;
+
+        pre = pre * nums[i];
+        suf = suf * nums[n - i - 1];
+        maxi = max(maxi, max(pre, suf));
+    }
+
+    return maxi;
+}
+
 int Merge(vector<int> &arr, int low, int mid, int high)
 {
     int left = low;
