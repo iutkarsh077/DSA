aa0862fc4d6ecb61f8edad21c1852eb8ee8c519d
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-09-15 07:24:19 +0530

    koko bananas

--- april2026/august2026/stl.cpp
+++ april2026/august2026/stl.cpp
@@ -385,6 +385,49 @@ int singleNonDuplicate(vector<int> &nums)
     return -1;
 }
 
+long long checker(vector<int> arr, int mid)
+{
+    long long total = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int ans1 = (arr[i] + (long long)mid - 1) / mid;
+        total += ans1;
+    }
+
+    return total;
+}
+int minEatingSpeed(vector<int> &piles, int h)
+{
+    int maxi = 0;
+
+    for (int i = 0; i < piles.size(); i++)
+    {
+        maxi = max(maxi, piles[i]);
+    }
+
+    int s = 1;
+    int e = maxi;
+
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+
+        long long ans = checker(piles, mid);
+
+        if (ans <= h)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+
+    return s;
+}
+
 int findPeakElement(vector<int> &nums)
 {
     if (nums.size() == 1 || nums[0] > nums[1])
