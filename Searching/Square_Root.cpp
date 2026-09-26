22f03a18cce640ad17947bf739432527578f9068
UTKARSH SINGH <119475896+iutkarsh077@users.noreply.github.com>
2026-09-26 22:35:12 +0530

    Add function to calculate square root using binary search
    Implement binary search to find square root of a number.

--- Searching/Square_Root.cpp
+++ Searching/Square_Root.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sqyareroot() {
+    int val = 4563;
+    int ans = 0;
+    
+    int s = 1;
+    int e = val;
+
+    while(s <= e){
+        long long int mid = s + (e - s)/2;
+
+        if(mid * mid <= val){
+            ans = mid;
+            s = mid + 1;
+        }
+
+        else {
+            e = mid - 1;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
+
 int main() {
     int val = 4563;
     int ans = 0;
